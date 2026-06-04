f21d317b13a5b85ceed8834958ba895617e47756
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-04 11:17:40 +0530

    Create 221-MaximalSquare

---
+++ 221-MaximalSquare/221-MaximalSquare.java
@@ -0,0 +1,23 @@
+// Last updated: 04/06/2026, 11:17:41
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int[][] dp=new int[n][m];
+        int maxSide=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]=='1'){
+                    if(i==0 || j==0){
+                        dp[i][j]=1;
+                    }
+                    else{
+                        dp[i][j]=1+Math.min(dp[i-1][j],Math.min(dp[i-1][j-1],dp[i][j-1]));
+                    }
+                    maxSide=Math.max(maxSide,dp[i][j]);
+                }
+            }
+        }
+        return maxSide*maxSide;
+    }
+}
