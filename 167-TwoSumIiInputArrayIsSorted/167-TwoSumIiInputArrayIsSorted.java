850df318e46c6e2181e7b0aed3470000ccf65236
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-04 11:18:22 +0530

    Create 167-TwoSumIiInputArrayIsSorted

---
+++ 167-TwoSumIiInputArrayIsSorted/167-TwoSumIiInputArrayIsSorted.java
@@ -0,0 +1,20 @@
+// Last updated: 04/06/2026, 11:18:23
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int n=numbers.length;
+        int l=0;
+        int r=n-1;
+        while(l<r){
+            if(numbers[l]+numbers[r]==target){
+                return new int[]{l+1,r+1};
+            }
+            else if(numbers[l]+numbers[r]>target){
+                r--;
+            }
+            else{
+                l++;
+            }
+        }
+        return new int[]{};
+    }
+}
