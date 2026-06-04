b57233f55d6f53c7bcd67f0f440ac82dcd41b1be
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-04 11:11:58 +0530

    Create 1960-CheckIfTheSentenceIsPangram

---
+++ 1960-CheckIfTheSentenceIsPangram/1960-CheckIfTheSentenceIsPangram.java
@@ -0,0 +1,13 @@
+// Last updated: 04/06/2026, 11:11:59
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        int[] arr=new int[26];
+        for(int i=0;i<sentence.length();i++){
+            arr[sentence.charAt(i)-'a']++;
+        }
+        for(int j=0;j<26;j++){
+            if(arr[j]==0)   return false;
+        }
+        return true;
+    }
+}
