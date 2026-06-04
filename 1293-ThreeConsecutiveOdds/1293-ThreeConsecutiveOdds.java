4f139874a225b1ab9bdba1f9bef2eb7cc2cb955a
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-04 11:13:06 +0530

    Create 1293-ThreeConsecutiveOdds

---
+++ 1293-ThreeConsecutiveOdds/1293-ThreeConsecutiveOdds.java
@@ -0,0 +1,11 @@
+// Last updated: 04/06/2026, 11:13:07
+class Solution {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        for(int i=0;i<arr.length-2;i++){
+            if(arr[i]%2==1 && arr[i+1]%2==1 && arr[i+2]%2==1){
+                return true;
+            }
+        }
+        return false;
+    }
+}
