1b10fedae2328a3ca588528da63bc554007b3d4d
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-04 11:20:01 +0530

    Create 59-SpiralMatrixIi

---
+++ 59-SpiralMatrixIi/59-SpiralMatrixIi.java
@@ -0,0 +1,31 @@
+// Last updated: 04/06/2026, 11:20:02
+class Solution {
+    public int[][] generateMatrix(int n) {
+        int[][] arr=new int[n][n];
+        int top=0,bottom=n-1,left=0,right=n-1;
+        int num=1;
+        while(top<=bottom && left<=right){
+            for(int i=left;i<=right;i++){
+                arr[top][i]=num++;
+            }
+            top++;
+            for(int i=top;i<=bottom;i++){
+                arr[i][right]=num++;
+            }
+            right--;
+            if(top<=bottom){
+                for(int i=right;i>=left;i--){
+                    arr[bottom][i]=num++;
+                }
+                bottom--;
+            }
+            if(left<=right){
+                for(int i=bottom;i>=top;i--){
+                    arr[i][left]=num++;
+                }
+                left++;
+            }
+        }
+        return arr;
+    }
+}
