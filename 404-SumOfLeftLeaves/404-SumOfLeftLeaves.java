443b790e7cd581006d3c3ec2c9ff0fe0ce603770
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-04 11:16:26 +0530

    Create 404-SumOfLeftLeaves

---
+++ 404-SumOfLeftLeaves/404-SumOfLeftLeaves.java
@@ -0,0 +1,27 @@
+// Last updated: 04/06/2026, 11:16:27
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+       return dfs(root,false);
+    }
+    int sum=0;
+    public int dfs(TreeNode root,boolean isLeft){
+        if(root==null)  return 0;
+        if(root.left==null && root.right==null && isLeft)  return root.val;
+        return dfs(root.left,true) + dfs(root.right,false);
+    }
+}
