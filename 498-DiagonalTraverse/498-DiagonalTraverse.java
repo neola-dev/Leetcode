ba31d32ed7b0176472e77167f6ccc95c066bf59b
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-04 11:15:54 +0530

    Create 498-DiagonalTraverse

---
+++ 498-DiagonalTraverse/498-DiagonalTraverse.java
@@ -0,0 +1,41 @@
+// Last updated: 04/06/2026, 11:15:55
+class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+       int n=mat.length;
+       int m=mat[0].length;
+       int[] arr=new int[n*m];
+       int r=0,c=0,dir=1; //1 up right -1 down left
+       for(int i=0;i<n*m;i++){
+            arr[i]=mat[r][c];
+            if(dir==1){
+                if(c==m-1){
+                    r++;
+                    dir=-1;
+                }
+                else if(r==0){
+                    c++;
+                    dir=-1;
+                }
+                else{
+                    r--;
+                    c++;
+                }
+            }
+            else{
+                if(r==n-1){
+                    c++;
+                    dir=1;
+                }
+                else if(c==0){
+                    r++;
+                    dir=1;
+                }
+                else{
+                    r++;
+                    c--;
+                }
+            }
+       } 
+       return arr;
+    }
+}
