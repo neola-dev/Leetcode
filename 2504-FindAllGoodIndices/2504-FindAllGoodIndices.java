ba808df9f8177613c1239d39c29fdd42ba0479f8
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-04 11:11:20 +0530

    Create 2504-FindAllGoodIndices

---
+++ 2504-FindAllGoodIndices/2504-FindAllGoodIndices.java
@@ -0,0 +1,32 @@
+// Last updated: 04/06/2026, 11:11:21
+class Solution {
+    public List<Integer> goodIndices(int[] nums, int k) {
+        int n=nums.length;
+        int[] left=new int[n];
+        int[] right=new int[n];
+        left[0]=1;
+        for(int i=1;i<n;i++){
+            if(nums[i]<=nums[i-1]){
+                left[i]=left[i-1]+1;
+            }else{
+                left[i]=1;
+            }
+        }
+        right[n-1]=1;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<=nums[i+1]){
+                right[i]=right[i+1]+1;
+            }
+            else{
+                right[i]=1;
+            }
+        }
+        List<Integer> ls=new ArrayList<>();
+        for(int i=k;i<n-k;i++){
+            if(left[i-1]>=k && right[i+1]>=k){
+                ls.add(i);
+            }
+        }
+        return ls;
+    }
+}
