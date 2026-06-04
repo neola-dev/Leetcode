633a6498a95c8beb45e968430f68259128daf975
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-04 11:12:58 +0530

    Create 1402-CountSquareSubmatricesWithAllOnes

---
+++ 1402-CountSquareSubmatricesWithAllOnes/1402-CountSquareSubmatricesWithAllOnes.java
@@ -0,0 +1,27 @@
+// Last updated: 04/06/2026, 11:12:59
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int[][] dp=new int[n][m];
+        //copy first row
+        for(int j=0;j<m;j++)    dp[0][j]=matrix[0][j];
+        //copy first col
+        for(int i=0;i<n;i++)    dp[i][0]=matrix[i][0];
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]==0) dp[i][j]=0;
+                else{
+                    dp[i][j]=1+Math.min(Math.min(dp[i-1][j],dp[i-1][j-1]),dp[i][j-1]);
+                }
+            }
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                sum+=dp[i][j];
+            }
+        }
+        return sum;
+    }
+}
