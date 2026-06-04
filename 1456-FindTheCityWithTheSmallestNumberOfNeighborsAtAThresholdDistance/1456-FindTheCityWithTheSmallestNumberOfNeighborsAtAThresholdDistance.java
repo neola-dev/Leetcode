fc8474f88bf5f4f25360a2532b0f5e90245ad7fa
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-04 11:12:43 +0530

    Create 1456-FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance

---
+++ 1456-FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance/1456-FindTheCityWithTheSmallestNumberOfNeighborsAtAThresholdDistance.java
@@ -0,0 +1,42 @@
+// Last updated: 04/06/2026, 11:12:44
+class Solution {
+    public int findTheCity(int n, int[][] edges, int distanceThreshold) {
+        int[][] dist=new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                dist[i][j]=Integer.MAX_VALUE;
+                if(i==j)    dist[i][j]=0;
+            }
+        }
+        for(int[] edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+            int cost=edge[2];
+            dist[u][v]=cost;
+            dist[v][u]=cost;
+        }
+        for(int via=0;via<n;via++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(dist[i][via]==Integer.MAX_VALUE || dist[via][j]==Integer.MAX_VALUE)continue;
+                    dist[i][j]=Math.min(dist[i][j],dist[i][via]+dist[via][j]);
+                }
+            }
+        }
+        int cntMax=n+1;
+        int res=-1;
+        for(int i=0;i<n;i++){
+            int cnt=0;
+            for(int j=0;j<n;j++){
+                if(dist[i][j]<=distanceThreshold){
+                    cnt++;
+                }
+            }
+            if(cnt<=cntMax){
+                cntMax=cnt;
+                res=i;
+            }
+        }
+        return res;
+    }
+}
