47a5fec4f091af672d86870adddbf6a8da3dff5b
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-04 11:20:15 +0530

    Create 50-PowxN

---
+++ 50-PowxN/50-PowxN.java
@@ -0,0 +1,20 @@
+// Last updated: 04/06/2026, 11:20:16
+class Solution {
+    public double myPow(double x, int n) {
+        double ans=1.0; 
+        long nn=n;
+        if(n<0) nn=-1*nn; 
+        while(nn>0){
+            if(nn%2==1){
+                ans=ans*x;
+                nn=nn-1;
+            }
+            else{
+                x=x*x;
+                nn=nn/2;
+            }
+        }
+        if(n<0) ans=(double)1.0/(double)ans;
+        return ans;
+    }
+}
