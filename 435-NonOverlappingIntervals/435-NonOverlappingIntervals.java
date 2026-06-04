d0a1f9bca35637eada49ecd0005ffc5513da2014
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-04 11:16:14 +0530

    Create 435-NonOverlappingIntervals

---
+++ 435-NonOverlappingIntervals/435-NonOverlappingIntervals.java
@@ -0,0 +1,17 @@
+// Last updated: 04/06/2026, 11:16:15
+class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)->a[1]-b[1]);
+        int cnt=0;
+        int prevEnd=intervals[0][1];
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0]<prevEnd){
+                cnt++;
+            }
+            else{
+                prevEnd=intervals[i][1];
+            }
+        }
+        return cnt;
+    }
+}
