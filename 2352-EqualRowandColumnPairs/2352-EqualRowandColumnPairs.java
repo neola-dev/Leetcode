a1c3a550ae25486d53c952b3b92ef2842e003483
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-05 13:17:34 +0530

    File updated at 05/06/2026, 13:17:35

--- 2352-EqualRowandColumnPairs/2352-EqualRowandColumnPairs.java
+++ 2352-EqualRowandColumnPairs/2352-EqualRowandColumnPairs.java
@@ -1,31 +1,21 @@
-// Last updated: 05/06/2026, 11:15:16
+// Last updated: 05/06/2026, 13:17:35
 1class Solution {
-2    public int equalPairs(int[][] grid) {
-3        HashMap<String,Integer> map=new HashMap<>();
-4        int n=grid.length;
-5        int i=0;
-6        while(i<n){
-7            String s="";
-8            for(int j=0;j<n;j++){
-9                s+=String.valueOf(grid[i][j]);
-10                s+="*";
-11            }
-12            map.put(s,map.getOrDefault(s,0)+1);
-13            i++;
-14        }
-15        int result=0;
-16        int j=0;
-17        while(j<n){
-18            String s="";
-19            for(int k=0;k<n;k++){
-20                s+=String.valueOf(grid[k][j]);
-21                s+="*";
-22            }
-23            if(map.containsKey(s)){
-24                result+=map.get(s);
-25            }
-26            j++;
-27        }
-28        return result;
-29    }
-30}
+2    public boolean closeStrings(String word1, String word2) {
+3        HashMap<Character,Integer> map1=new HashMap<>();
+4        HashMap<Character,Integer> map2=new HashMap<>();
+5        for(char ch:word1.toCharArray()){
+6            map1.put(ch,map1.getOrDefault(ch,0)+1);
+7        }
+8        for(char ch:word2.toCharArray()){
+9            map2.put(ch,map2.getOrDefault(ch,0)+1);
+10        }
+11        if(!map1.keySet().equals(map2.keySet()))    return false;
+12        List<Integer> list1=new ArrayList<>();
+13        for(int val:map1.values())  list1.add(val);
+14        List<Integer> list2=new ArrayList<>();
+15        for(int val:map2.values())  list2.add(val);
+16        Collections.sort(list1);
+17        Collections.sort(list2);
+18        return list1.equals(list2);
+19    }
+20}
