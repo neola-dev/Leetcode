173eccb8f025964e1f6a7fca7e05207e690e537a
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-05 09:29:05 +0530

    File updated at 05/06/2026, 09:29:06

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
@@ -1,16 +1,19 @@
-// Last updated: 04/06/2026, 11:15:30
-class Solution {
-    public int subarraySum(int[] nums, int k) {
-        int cnt=0;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        map.put(0,1);
-        int presum=0;
-        for(int num:nums){
-            presum+=num;
-            int rem=presum-k;
-            cnt+=map.getOrDefault(rem,0);
-            map.put(presum,map.getOrDefault(presum,0)+1);
-        }
-        return cnt;
-    }
-}
+// Last updated: 05/06/2026, 09:29:06
+1class Solution {
+2    public boolean checkSubarraySum(int[] nums, int k) {
+3        HashMap<Integer,Integer> map=new HashMap<>();
+4        map.put(0,-1);
+5        int preSum=0;
+6        for(int i=0;i<nums.length;i++){
+7            preSum+=nums[i];
+8            int rem=preSum%k;
+9            if(map.containsKey(rem)){
+10                if(i-map.get(rem)>=2)   return true;
+11            }
+12            else{
+13                map.put(rem,i);
+14            }
+15        }
+16        return false;
+17    }
+18}
