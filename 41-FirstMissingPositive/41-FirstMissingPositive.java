ba9e38b50df157f24a10bf47bb43393c8cacc941
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-06 09:27:05 +0530

    File updated at 06/06/2026, 09:27:06

--- 41-FirstMissingPositive/41-FirstMissingPositive.java
+++ 41-FirstMissingPositive/41-FirstMissingPositive.java
@@ -1,17 +1,24 @@
-// Last updated: 06/06/2026, 09:06:35
+// Last updated: 06/06/2026, 09:27:06
 1class Solution {
 2    public int firstMissingPositive(int[] nums) {
-3        HashMap<Integer,Integer> map=new HashMap<>();
-4        int last=Integer.MIN_VALUE;
-5        for(int i=0;i<nums.length;i++){
-6            map.put(nums[i],i);
-7            last=Math.max(nums[i],last);
-8        }
-9        for(int i=1;i<=nums.length;i++){
-10            if(!map.containsKey(i)){
-11                return i;
-12            }
-13        }
-14        return last+1;
-15    }
-16}
+3        int n=nums.length;
+4        int i=0;
+5        while(i<n){
+6            int crt=nums[i]-1;
+7            if(nums[i]>0  && nums[i]<=n && nums[i]!=nums[crt]){
+8                int temp=nums[i];
+9                nums[i]=nums[crt];
+10                nums[crt]=temp;
+11            }
+12            else{
+13                i++;
+14            }
+15        }
+16        for(int j=0;j<n;j++){
+17            if(nums[j]!=j+1){
+18                return j+1;
+19            }
+20        }
+21        return n+1;
+22    }
+23}
