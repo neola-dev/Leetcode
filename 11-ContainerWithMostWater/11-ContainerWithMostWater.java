a8f28f3891cb02c29c1ca920bfe9996d3e9e6fdc
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-09 11:29:50 +0530

    File updated at 09/06/2026, 11:29:53

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,30 +1,27 @@
-// Last updated: 09/06/2026, 11:29:07
+// Last updated: 09/06/2026, 11:29:53
 1class Solution {
 2    public boolean checkInclusion(String s1, String s2) {
 3        int[] freq=new int[26];
 4        for(int i=0;i<s1.length();i++){
-5            char ch=s1.charAt(i);
-6            freq[ch-'a']++;
-7        }
+5            freq[s1.charAt(i)-'a']++;
+6        }
+7        int cnt=s1.length();
 8        int l=0,r=0;
-9        int cnt=s1.length();
-10        while(r<s2.length()){
-11            char chR=s2.charAt(r);
-12            if(freq[chR-'a']>0){
-13                cnt--;
-14            }
-15            freq[chR-'a']--;
-16            r++;
-17            if(r-l==s1.length()){
-18                char chL=s2.charAt(l);
-19                if(cnt==0)  return true;
-20                if(freq[chL-'a']>=0){
-21                    cnt++;
-22                }
-23                freq[chL-'a']++;
-24                l++;
-25            }
-26        }
-27        return false;
-28    }
-29}
+9        while(r<s2.length()){
+10            char chR=s2.charAt(r);
+11            if(freq[chR-'a']>0){
+12                cnt--;
+13            }
+14            freq[chR-'a']--;
+15            if(r-l+1==s1.length()){
+16                char chL=s2.charAt(l);
+17                if(cnt==0)  return true;
+18                if(freq[chL-'a']>=0)    cnt++;
+19                freq[chL-'a']++;
+20                l++;
+21            }
+22            r++;
+23        }
+24        return false;
+25    }
+26}
