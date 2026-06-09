d5a898e4ed74f0b8c45656117715929be46c9093
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-09 11:48:34 +0530

    File updated at 09/06/2026, 11:48:37

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,27 +1,18 @@
-// Last updated: 09/06/2026, 11:29:53
+// Last updated: 09/06/2026, 11:48:37
 1class Solution {
-2    public boolean checkInclusion(String s1, String s2) {
-3        int[] freq=new int[26];
-4        for(int i=0;i<s1.length();i++){
-5            freq[s1.charAt(i)-'a']++;
-6        }
-7        int cnt=s1.length();
-8        int l=0,r=0;
-9        while(r<s2.length()){
-10            char chR=s2.charAt(r);
-11            if(freq[chR-'a']>0){
-12                cnt--;
-13            }
-14            freq[chR-'a']--;
-15            if(r-l+1==s1.length()){
-16                char chL=s2.charAt(l);
-17                if(cnt==0)  return true;
-18                if(freq[chL-'a']>=0)    cnt++;
-19                freq[chL-'a']++;
-20                l++;
-21            }
-22            r++;
-23        }
-24        return false;
-25    }
-26}
+2    public int[] maxSlidingWindow(int[] nums, int k) {
+3       List<Integer> res=new ArrayList<>();
+4       Deque<Integer> dq=new LinkedList<>();
+5       for(int i=0;i<nums.length;i++){
+6            while(!dq.isEmpty() && dq.peekFirst()<=i-k) dq.pollFirst();
+7            while(!dq.isEmpty() && nums[dq.peekLast()]<nums[i]) dq.pollLast();
+8            dq.offerLast(i);
+9            if(i>=k-1)  res.add(nums[dq.peekFirst()]);
+10       } 
+11       int[] ans=new int[res.size()];
+12       for(int i=0;i<res.size();i++){
+13            ans[i]=res.get(i);
+14       }
+15       return ans;
+16    }
+17}
