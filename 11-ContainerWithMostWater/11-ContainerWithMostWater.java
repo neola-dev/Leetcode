3cf2ba583fc18f3d82f37ce97d615b4eca39c872
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-09 11:09:38 +0530

    File updated at 09/06/2026, 11:09:41

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,19 +1,24 @@
-// Last updated: 09/06/2026, 11:06:53
+// Last updated: 09/06/2026, 11:09:41
 1class Solution {
 2    public int maxVowels(String s, int k) {
-3        int vowelCnt=0;
-4        int maxCnt=0;
-5        int l=0;
-6        for(int r=0;r<s.length();r++){
+3        int n=s.length();
+4        int l=0,r=0;
+5        int maxLen=0,vowelCnt=0;
+6        while(r<n){
 7            char ch=s.charAt(r);
-8            if(ch=='a' || ch=='e' || ch=='i'|| ch=='o' || ch=='u')  vowelCnt++;
-9            if((r-l+1)==k){
-10                maxCnt=Math.max(maxCnt,vowelCnt);
-11                char chL=s.charAt(l);
-12                if(chL=='a' || chL=='e' || chL=='i'|| chL=='o' || chL=='u')  vowelCnt--;
-13                l++;
-14            }
-15        }
-16        return maxCnt;
-17    }
-18}
+8            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+9                vowelCnt++;
+10            }
+11            if((r-l+1)==k){
+12                maxLen=Math.max(maxLen,vowelCnt);
+13                char chL=s.charAt(l);
+14                if(chL=='a' || chL=='e' || chL=='i' || chL=='o' || chL=='u'){
+15                    vowelCnt--;
+16                }
+17                l++;
+18            }
+19            r++;
+20        }
+21        return maxLen;
+22    }
+23}
