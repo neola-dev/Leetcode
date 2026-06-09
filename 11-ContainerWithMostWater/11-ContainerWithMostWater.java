daec77ad68343d29d43289b783dbed94aec0a09a
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-09 11:29:04 +0530

    File updated at 09/06/2026, 11:29:07

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,24 +1,30 @@
-// Last updated: 09/06/2026, 11:09:41
+// Last updated: 09/06/2026, 11:29:07
 1class Solution {
-2    public int maxVowels(String s, int k) {
-3        int n=s.length();
-4        int l=0,r=0;
-5        int maxLen=0,vowelCnt=0;
-6        while(r<n){
-7            char ch=s.charAt(r);
-8            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
-9                vowelCnt++;
-10            }
-11            if((r-l+1)==k){
-12                maxLen=Math.max(maxLen,vowelCnt);
-13                char chL=s.charAt(l);
-14                if(chL=='a' || chL=='e' || chL=='i' || chL=='o' || chL=='u'){
-15                    vowelCnt--;
-16                }
-17                l++;
-18            }
-19            r++;
-20        }
-21        return maxLen;
-22    }
-23}
+2    public boolean checkInclusion(String s1, String s2) {
+3        int[] freq=new int[26];
+4        for(int i=0;i<s1.length();i++){
+5            char ch=s1.charAt(i);
+6            freq[ch-'a']++;
+7        }
+8        int l=0,r=0;
+9        int cnt=s1.length();
+10        while(r<s2.length()){
+11            char chR=s2.charAt(r);
+12            if(freq[chR-'a']>0){
+13                cnt--;
+14            }
+15            freq[chR-'a']--;
+16            r++;
+17            if(r-l==s1.length()){
+18                char chL=s2.charAt(l);
+19                if(cnt==0)  return true;
+20                if(freq[chL-'a']>=0){
+21                    cnt++;
+22                }
+23                freq[chL-'a']++;
+24                l++;
+25            }
+26        }
+27        return false;
+28    }
+29}
