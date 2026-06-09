dbab432778480e5b2b54e08f0343ec9663d371f7
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-09 11:06:50 +0530

    File updated at 09/06/2026, 11:06:53

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,22 +1,19 @@
-// Last updated: 09/06/2026, 09:34:35
+// Last updated: 09/06/2026, 11:06:53
 1class Solution {
-2    public int trap(int[] height) {
-3        int n=height.length;
-4        int l=0;
-5        int r=n-1;
-6        int tot=0,maxLeft=0,maxRight=0;
-7        while(l<r){
-8            if(height[l]<=height[r]){
-9                if(height[l]>maxLeft)   maxLeft=height[l];
-10                else tot+=maxLeft-height[l];
-11                l++;
-12            }
-13            else{
-14                if(height[r]>maxRight)  maxRight=height[r];
-15                else tot+=maxRight-height[r];
-16                r--;
-17            }
-18        }
-19        return tot;
-20    }
-21}
+2    public int maxVowels(String s, int k) {
+3        int vowelCnt=0;
+4        int maxCnt=0;
+5        int l=0;
+6        for(int r=0;r<s.length();r++){
+7            char ch=s.charAt(r);
+8            if(ch=='a' || ch=='e' || ch=='i'|| ch=='o' || ch=='u')  vowelCnt++;
+9            if((r-l+1)==k){
+10                maxCnt=Math.max(maxCnt,vowelCnt);
+11                char chL=s.charAt(l);
+12                if(chL=='a' || chL=='e' || chL=='i'|| chL=='o' || chL=='u')  vowelCnt--;
+13                l++;
+14            }
+15        }
+16        return maxCnt;
+17    }
+18}
