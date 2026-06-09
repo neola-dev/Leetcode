aa4665e76e92c1951dab7ce51b04eefbf0440222
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-09 09:34:32 +0530

    File updated at 09/06/2026, 09:34:35

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,22 +1,22 @@
-// Last updated: 09/06/2026, 09:13:18
+// Last updated: 09/06/2026, 09:34:35
 1class Solution {
-2    public int maxArea(int[] height) {
+2    public int trap(int[] height) {
 3        int n=height.length;
 4        int l=0;
 5        int r=n-1;
-6        int maxArea=Integer.MIN_VALUE;
+6        int tot=0,maxLeft=0,maxRight=0;
 7        while(l<r){
-8            int area=Math.min(height[l],height[r]);
-9            int dist=r-l;
-10            int totArea=area*dist;
-11            maxArea=Math.max(maxArea,totArea);
-12            if(height[l]<height[r]){
-13                l++;
-14            }
-15            else{
+8            if(height[l]<=height[r]){
+9                if(height[l]>maxLeft)   maxLeft=height[l];
+10                else tot+=maxLeft-height[l];
+11                l++;
+12            }
+13            else{
+14                if(height[r]>maxRight)  maxRight=height[r];
+15                else tot+=maxRight-height[r];
 16                r--;
 17            }
 18        }
-19        return maxArea;
+19        return tot;
 20    }
 21}
