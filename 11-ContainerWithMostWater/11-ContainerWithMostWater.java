3c17f11548222ee227cacbd712a5e24fcdeb5cef
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-09 12:36:14 +0530

    File updated at 09/06/2026, 12:36:17

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,18 +1,23 @@
-// Last updated: 09/06/2026, 11:48:37
+// Last updated: 09/06/2026, 12:36:17
 1class Solution {
-2    public int[] maxSlidingWindow(int[] nums, int k) {
-3       List<Integer> res=new ArrayList<>();
-4       Deque<Integer> dq=new LinkedList<>();
-5       for(int i=0;i<nums.length;i++){
-6            while(!dq.isEmpty() && dq.peekFirst()<=i-k) dq.pollFirst();
-7            while(!dq.isEmpty() && nums[dq.peekLast()]<nums[i]) dq.pollLast();
-8            dq.offerLast(i);
-9            if(i>=k-1)  res.add(nums[dq.peekFirst()]);
-10       } 
-11       int[] ans=new int[res.size()];
-12       for(int i=0;i<res.size();i++){
-13            ans[i]=res.get(i);
-14       }
-15       return ans;
-16    }
-17}
+2    public int characterReplacement(String s, int k) {
+3       int l=0,r=0;
+4       int n=s.length();
+5       int[] freq=new int[26];
+6       int maxLen=1;
+7       int maxFreq=1;
+8       while(r<n){
+9            char chR=s.charAt(r);
+10            freq[chR-'A']++;
+11            maxFreq=Math.max(maxFreq,freq[chR-'A']);
+12            if((r-l+1)-maxFreq>k){
+13                char chL=s.charAt(l);
+14                freq[chL-'A']--;
+15                l++;
+16            }
+17            maxLen=Math.max(maxLen,(r-l+1));
+18            r++;
+19       } 
+20       return maxLen;
+21    }
+22}
