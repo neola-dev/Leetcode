d781163828bda2a3f836912a762b020f22705c91
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-09 09:13:15 +0530

    File updated at 09/06/2026, 09:13:18

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,18 +1,22 @@
-// Last updated: 08/06/2026, 23:26:59
+// Last updated: 09/06/2026, 09:13:18
 1class Solution {
-2    public int totalFruit(int[] fruits) {
-3        HashMap<Integer,Integer> map=new HashMap<>();
-4        int l=0,r=0,max=0;
-5        while(r<fruits.length){
-6            map.put(fruits[r],map.getOrDefault(fruits[r],0)+1);
-7            while(map.size()>2){
-8                map.put(fruits[l],map.get(fruits[l])-1);
-9                if(map.get(fruits[l])<=0)   map.remove(fruits[l]);
-10                l++;
-11            }
-12            max=Math.max(max,(r-l+1));
-13            r++;
-14        }
-15        return max;
-16    }
-17}
+2    public int maxArea(int[] height) {
+3        int n=height.length;
+4        int l=0;
+5        int r=n-1;
+6        int maxArea=Integer.MIN_VALUE;
+7        while(l<r){
+8            int area=Math.min(height[l],height[r]);
+9            int dist=r-l;
+10            int totArea=area*dist;
+11            maxArea=Math.max(maxArea,totArea);
+12            if(height[l]<height[r]){
+13                l++;
+14            }
+15            else{
+16                r--;
+17            }
+18        }
+19        return maxArea;
+20    }
+21}
