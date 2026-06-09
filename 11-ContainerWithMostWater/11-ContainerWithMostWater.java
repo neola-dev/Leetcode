12ea9903d97359fd91c000ca3d9e14096155cb7a
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-09 16:54:45 +0530

    File updated at 09/06/2026, 16:54:48

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,20 +1,20 @@
-// Last updated: 09/06/2026, 16:50:49
+// Last updated: 09/06/2026, 16:54:48
 1class Solution {
-2    public int minSubArrayLen(int target, int[] nums) {
-3        int l=0;
-4        int n=nums.length;
+2    public int longestSubarray(int[] nums) {
+3        int n=nums.length;
+4        int l=0;
 5        int r=0;
-6        int min=Integer.MAX_VALUE;
-7        int sum=0;
+6        int max=0;
+7        int zero=0;
 8        while(r<n){
-9            sum+=nums[r];
-10            while(sum>=target){
-11                min=Math.min(min,r-l+1);
-12                sum-=nums[l];
-13                l++;
-14            }
+9            if(nums[r]==0)  zero++;
+10            while(zero>1){
+11               if(nums[l]==0)   zero--;
+12               l++; 
+13            }
+14            max=Math.max(max,r-l);
 15            r++;
 16        }
-17        return min==Integer.MAX_VALUE?0:min;
+17        return max;
 18    }
 19}
