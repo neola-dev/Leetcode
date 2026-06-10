198886ed301cb0be27f1f7f37b724a56a98442ea
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-10 12:35:30 +0530

    File updated at 10/06/2026, 12:35:34

--- 76-MinimumWindowSubstring/76-MinimumWindowSubstring.java
+++ 76-MinimumWindowSubstring/76-MinimumWindowSubstring.java
@@ -1,41 +1,29 @@
-// Last updated: 10/06/2026, 12:20:43
-1class MinStack {
-2    Stack<Integer> st;
-3    Stack<Integer> minSt;
-4    public MinStack() {
-5        st=new Stack<>();
-6        minSt=new Stack<>();
-7    }
-8    
-9    public void push(int value) {
-10        st.push(value);
-11        if(minSt.isEmpty()){
-12            minSt.push(value);
-13        }
-14        else{
-15            minSt.push(Math.min(value,minSt.peek()));
-16        }
-17    }
-18    
-19    public void pop() {
-20        st.pop();
-21        minSt.pop();
-22    }
-23    
-24    public int top() {
-25        return st.peek();
-26    }
-27    
-28    public int getMin() {
-29        return minSt.peek();
-30    }
-31}
-32
-33/**
-34 * Your MinStack object will be instantiated and called as such:
-35 * MinStack obj = new MinStack();
-36 * obj.push(value);
-37 * obj.pop();
-38 * int param_3 = obj.top();
-39 * int param_4 = obj.getMin();
-40 */
+// Last updated: 10/06/2026, 12:35:34
+1class Solution {
+2    public int evalRPN(String[] tokens) {
+3        int ans=0;
+4        Stack<Integer> st=new Stack<>();
+5        for(String str:tokens){
+6            if(str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/")){
+7                int first=st.pop();
+8                int second=st.pop();
+9                if(str.equals("+")){
+10                    st.push((second+first));
+11                }
+12                else if(str.equals("-")){
+13                    st.push((second-first));
+14                }
+15                else if(str.equals("/")){
+16                    st.push((second/first));
+17                }
+18                else if(str.equals("*")){
+19                    st.push((second*first));
+20                }
+21            }
+22            else{
+23                st.push(Integer.parseInt(str));
+24            }
+25        }
+26        return st.pop();
+27    }
+28}
