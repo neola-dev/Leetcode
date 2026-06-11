5315c5234a4b1bee3ac9b3f1a9a4811d0dbe671c
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-11 13:21:40 +0530

    File updated at 11/06/2026, 13:21:41

--- 739-DailyTemperatures/739-DailyTemperatures.java
+++ 739-DailyTemperatures/739-DailyTemperatures.java
@@ -1,22 +1,18 @@
-// Last updated: 11/06/2026, 13:17:08
-1class StockSpanner {
-2    Stack<int[]> st;
-3    public StockSpanner() {
-4        st=new Stack<>();
-5    }
-6    
-7    public int next(int price) {
-8        int span=1;
-9        while(!st.isEmpty() && st.peek()[0]<=price){
-10            span+=st.pop()[1];
-11        }
-12        st.push(new int[]{price,span});
-13        return span;
-14    }
-15}
-16
-17/**
-18 * Your StockSpanner object will be instantiated and called as such:
-19 * StockSpanner obj = new StockSpanner();
-20 * int param_1 = obj.next(price);
-21 */
+// Last updated: 11/06/2026, 13:21:41
+1class Solution {
+2    public int[] nextGreaterElements(int[] nums) {
+3        int n=nums.length;
+4        int[] nge=new int[n];
+5        Stack<Integer> st=new Stack<>();
+6        for(int i=n*2-1;i>=0;i--){
+7            if(i<n){
+8                while(!st.isEmpty() && nums[st.peek()%n]<=nums[i]){
+9                    st.pop();
+10                }
+11                nge[i]=st.isEmpty()?-1:nums[st.peek()%n];
+12            }
+13            st.push(i);
+14        }
+15        return nge;
+16    }
+17}
