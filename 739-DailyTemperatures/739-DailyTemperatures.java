d98d64436c5c7f1a19d254236c0b5419f59ea96c
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-11 17:14:41 +0530

    File updated at 11/06/2026, 17:14:42

--- 739-DailyTemperatures/739-DailyTemperatures.java
+++ 739-DailyTemperatures/739-DailyTemperatures.java
@@ -1,18 +1,31 @@
-// Last updated: 11/06/2026, 13:23:28
+// Last updated: 11/06/2026, 17:14:42
 1class Solution {
-2    public int[] nextGreaterElements(int[] nums) {
-3        int n=nums.length;
-4        int[] nge=new int[n];
-5        Stack<Integer> st=new Stack<>();
-6        for(int i=n*2-1;i>=0;i--){
-7            if(i<n){
-8                while(!st.isEmpty() && nums[i]>=nums[st.peek()%n]){
+2    public int[] asteroidCollision(int[] asteroids) {
+3        Stack<Integer> st=new Stack<>();
+4        int n=asteroids.length;
+5        for(int i=0;i<n;i++){
+6            if(asteroids[i]>0)  st.push(asteroids[i]);
+7            else{
+8                while(!st.isEmpty() && st.peek()>0 && st.peek()<Math.abs(asteroids[i])){
 9                    st.pop();
 10                }
-11                nge[i]=st.isEmpty()?-1:nums[st.peek()%n];
-12            }
-13            st.push(i);
-14        }
-15        return nge;
-16    }
-17}
+11                if(!st.isEmpty() && st.peek()==Math.abs(asteroids[i])){
+12                    st.pop();
+13                }
+14                else if(st.isEmpty() || st.peek()<0){
+15                    st.push(asteroids[i]);
+16                }
+17            }    
+18        }
+19        List<Integer> lis=new ArrayList<>();
+20        while(!st.isEmpty()){
+21            lis.add(st.pop());
+22        }
+23        int m=lis.size();
+24        int[] res=new int[m];
+25        for(int i=0;i<res.length;i++){
+26            res[i]=lis.get(m-i-1);
+27        }
+28        return res;
+29    }
+30}
