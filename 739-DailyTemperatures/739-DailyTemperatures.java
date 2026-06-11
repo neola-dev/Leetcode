21d0a251697c5bb099f8a5917afabf7f59525aae
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-11 11:46:46 +0530

    File updated at 11/06/2026, 11:46:47

--- 739-DailyTemperatures/739-DailyTemperatures.java
+++ 739-DailyTemperatures/739-DailyTemperatures.java
@@ -1,16 +1,16 @@
-// Last updated: 04/06/2026, 11:14:46
-class Solution {
-    public int[] dailyTemperatures(int[] temp) {
-        int n=temp.length;
-        int[] nge=new int[n];
-        Stack<Integer> st=new Stack<>();
-        for(int i=n-1;i>=0;i--){
-            while(!st.isEmpty() && temp[st.peek()]<=temp[i]){
-                st.pop();
-            }
-            nge[i]=st.isEmpty()?0:st.peek()-i;
-            st.add(i);
-        }
-        return nge;
-    }
-}
+// Last updated: 11/06/2026, 11:46:47
+1class Solution {
+2    public int[] dailyTemperatures(int[] temp) {
+3        int n=temp.length;
+4        Stack<Integer> st=new Stack<>();
+5        int[] ans=new int[n];
+6        for(int i=n-1;i>=0;i--){
+7            while(!st.isEmpty() && temp[i]>=temp[st.peek()]){
+8                st.pop();
+9            }
+10            ans[i]=st.isEmpty()?0:st.peek()-i;
+11            st.push(i);
+12        }
+13        return ans;
+14    }
+15}
