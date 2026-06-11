3b8b0cbb8e0985fe9131f933dfcfdfb4981a82e8
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-11 17:15:20 +0530

    File updated at 11/06/2026, 17:15:21

--- 739-DailyTemperatures/739-DailyTemperatures.java
+++ 739-DailyTemperatures/739-DailyTemperatures.java
@@ -1,31 +1,29 @@
-// Last updated: 11/06/2026, 17:14:42
+// Last updated: 11/06/2026, 17:15:21
 1class Solution {
-2    public int[] asteroidCollision(int[] asteroids) {
+2    public int[] asteroidCollision(int[] aster) {
 3        Stack<Integer> st=new Stack<>();
-4        int n=asteroids.length;
-5        for(int i=0;i<n;i++){
-6            if(asteroids[i]>0)  st.push(asteroids[i]);
-7            else{
-8                while(!st.isEmpty() && st.peek()>0 && st.peek()<Math.abs(asteroids[i])){
-9                    st.pop();
-10                }
-11                if(!st.isEmpty() && st.peek()==Math.abs(asteroids[i])){
-12                    st.pop();
-13                }
-14                else if(st.isEmpty() || st.peek()<0){
-15                    st.push(asteroids[i]);
-16                }
-17            }    
-18        }
-19        List<Integer> lis=new ArrayList<>();
-20        while(!st.isEmpty()){
-21            lis.add(st.pop());
-22        }
-23        int m=lis.size();
-24        int[] res=new int[m];
-25        for(int i=0;i<res.length;i++){
-26            res[i]=lis.get(m-i-1);
-27        }
-28        return res;
-29    }
-30}
+4        for(int num:aster){
+5            if(num>0)   st.push(num);
+6            else{
+7                while(!st.isEmpty() && st.peek()>0 && st.peek()<Math.abs(num)){
+8                    st.pop();
+9                }
+10                if(!st.isEmpty() && st.peek()==Math.abs(num)){
+11                    st.pop();
+12                }
+13                else if(st.isEmpty() || st.peek()<0){
+14                    st.push(num);
+15                }
+16            }
+17        }
+18        List<Integer> res=new ArrayList<>();
+19        while(!st.isEmpty()){
+20            res.add(st.pop());
+21        }
+22        int[] ans=new int[res.size()];
+23        for(int i=0;i<res.size();i++){
+24            ans[i]=res.get(res.size()-1-i);
+25        }
+26        return ans;
+27    }
+28}
