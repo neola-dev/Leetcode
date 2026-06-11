185758471849290fe1627d0df379624a7244e42e
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-11 11:59:20 +0530

    File updated at 11/06/2026, 11:59:21

--- 739-DailyTemperatures/739-DailyTemperatures.java
+++ 739-DailyTemperatures/739-DailyTemperatures.java
@@ -1,16 +1,21 @@
-// Last updated: 11/06/2026, 11:46:47
+// Last updated: 11/06/2026, 11:59:21
 1class Solution {
-2    public int[] dailyTemperatures(int[] temp) {
-3        int n=temp.length;
-4        Stack<Integer> st=new Stack<>();
-5        int[] ans=new int[n];
+2    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+3        HashMap<Integer,Integer> map=new HashMap<>();
+4        int n=nums2.length;
+5        Stack<Integer> st=new Stack<>();
 6        for(int i=n-1;i>=0;i--){
-7            while(!st.isEmpty() && temp[i]>=temp[st.peek()]){
+7            while(!st.isEmpty() && nums2[i]>nums2[st.peek()]){
 8                st.pop();
 9            }
-10            ans[i]=st.isEmpty()?0:st.peek()-i;
-11            st.push(i);
-12        }
-13        return ans;
-14    }
-15}
+10            int val=st.isEmpty()?-1:nums2[st.peek()];
+11            map.put(nums2[i],val);
+12            st.push(i);
+13        }
+14        int[] ans=new int[nums1.length];
+15        for(int i=0;i<nums1.length;i++){
+16            ans[i]=map.get(nums1[i]);
+17        }
+18        return ans;
+19    }
+20}
