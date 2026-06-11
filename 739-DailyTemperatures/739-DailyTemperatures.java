0c07698f148c0d73249d6e45919b3fe351f304b3
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-11 13:23:28 +0530

    File updated at 11/06/2026, 13:23:28

--- 739-DailyTemperatures/739-DailyTemperatures.java
+++ 739-DailyTemperatures/739-DailyTemperatures.java
@@ -1,4 +1,4 @@
-// Last updated: 11/06/2026, 13:21:41
+// Last updated: 11/06/2026, 13:23:28
 1class Solution {
 2    public int[] nextGreaterElements(int[] nums) {
 3        int n=nums.length;
@@ -6,7 +6,7 @@
 5        Stack<Integer> st=new Stack<>();
 6        for(int i=n*2-1;i>=0;i--){
 7            if(i<n){
-8                while(!st.isEmpty() && nums[st.peek()%n]<=nums[i]){
+8                while(!st.isEmpty() && nums[i]>=nums[st.peek()%n]){
 9                    st.pop();
 10                }
 11                nge[i]=st.isEmpty()?-1:nums[st.peek()%n];
