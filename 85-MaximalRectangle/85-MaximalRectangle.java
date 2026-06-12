6bddfbc24e54faa316dea8b3a0341bc8eca3d7f0
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-12 23:04:29 +0530

    File updated at 12/06/2026, 23:04:29

--- 85-MaximalRectangle/85-MaximalRectangle.java
+++ 85-MaximalRectangle/85-MaximalRectangle.java
@@ -1,47 +1,48 @@
-// Last updated: 04/06/2026, 11:19:34
-class Solution {
-    public int largestRectangle(int[] heights){
-        Stack<Integer> st=new Stack<>();
-        int n=heights.length;
-        int[] pse=new int[n];
-        for(int i=0;i<n;i++){
-            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
-                st.pop();
-            }
-            pse[i]=st.isEmpty()?-1:st.peek();
-            st.push(i);
-        }
-        st.clear();
-        int[] nse=new int[n];
-        for(int i=n-1;i>=0;i--){
-            while(!st.isEmpty() && heights[st.peek()]>=heights[i]){
-                st.pop();
-            }
-            nse[i]=st.isEmpty()?n:st.peek();
-            st.push(i);
-        }
-        int maxArea=0;
-        for(int i=0;i<n;i++){
-            int area=heights[i]*(nse[i]-pse[i]-1);
-            maxArea=Math.max(maxArea,area);
-        }
-        return maxArea;
-    }
-    public int maximalRectangle(char[][] matrix) {
-        int maxArea=0;
-        int[] heights=new int[matrix[0].length];
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[i].length;j++){
-                if(matrix[i][j]=='1'){
-                    heights[j]++;
-                }
-                else{
-                    heights[j]=0;
-                }
-            }
-            int area=largestRectangle(heights);
-            maxArea=Math.max(maxArea,area);
-        }
-        return maxArea;
-    }
-}
+// Last updated: 12/06/2026, 23:04:29
+1class Solution {
+2    public int findLarge(int[] hei){
+3        int n=hei.length;
+4        int[] pse=new int[n];
+5        int[] nse=new int[n];
+6        Stack<Integer> st=new Stack<>();
+7        for(int i=0;i<n;i++){
+8            while(!st.isEmpty() && hei[i]<=hei[st.peek()]){
+9                st.pop();
+10            }
+11            pse[i]=st.isEmpty()?-1:st.peek();
+12            st.push(i);
+13        }
+14        st.clear();
+15        for(int i=n-1;i>=0;i--){
+16            while(!st.isEmpty() && hei[i]<=hei[st.peek()]){
+17                st.pop();
+18            }
+19            nse[i]=st.isEmpty()?n:st.peek();
+20            st.push(i);
+21        }
+22        int maxArea=0;
+23        for(int i=0;i<n;i++){
+24            int area=hei[i]*(nse[i]-pse[i]-1);
+25            maxArea=Math.max(maxArea,area);
+26        }
+27        return maxArea;
+28    }
+29    public int maximalRectangle(char[][] matrix) {
+30        int m=matrix[0].length;
+31        int[] heights=new int[m];
+32        int maxArea=Integer.MIN_VALUE;
+33        for(int i=0;i<matrix.length;i++){
+34            for(int j=0;j<matrix[0].length;j++){
+35                if(matrix[i][j]=='1'){
+36                    heights[j]++;
+37                }
+38                else{
+39                    heights[j]=0;
+40                }
+41            }
+42            int larg=findLarge(heights);
+43            maxArea=Math.max(maxArea,larg);
+44        }
+45        return maxArea;
+46    }
+47}
