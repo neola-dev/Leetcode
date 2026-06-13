ed4a7010f0186412f9497ccd7129d0774b0835e3
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-13 12:35:44 +0530

    File updated at 13/06/2026, 12:35:46

--- 42-TrappingRainWater/42-TrappingRainWater.java
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -1,43 +1,40 @@
-// Last updated: 13/06/2026, 12:03:52
+// Last updated: 13/06/2026, 12:35:46
 1class Solution {
-2     public int[] findNse(int[] arr){
-3        int n=arr.length;
-4        int[] nse=new int[n];
-5        Stack<Integer> st=new Stack<>();
-6        for(int i=n-1;i>=0;i--){
-7            while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
-8                st.pop();
-9            }
-10            if(st.isEmpty())    nse[i]=n;
-11            else    nse[i]=st.peek();
+2    public long subArrayRanges(int[] nums) {
+3        int n = nums.length;
+4        int[] pge = new int[n];
+5        int[] nge = new int[n];
+6        int[] pse = new int[n];
+7        int[] nse = new int[n];
+8        Stack<Integer> st = new Stack<>();
+9        for (int i = 0; i < n; i++) {
+10            while (!st.isEmpty() && nums[st.peek()]<nums[i]) st.pop();
+11            pge[i] = st.isEmpty() ? -1 : st.peek();
 12            st.push(i);
 13        }
-14        return nse;
-15    }
-16    public int[] findPse(int[] arr){
-17        int n=arr.length;
-18        int[] pse=new int[n];
-19        Stack<Integer> st=new Stack<>();
-20        for(int i=0;i<n;i++){
-21            while(!st.isEmpty() && arr[st.peek()]>arr[i]){
-22                st.pop();
-23            }
-24            if(st.isEmpty())    pse[i]=-1;
-25            else    pse[i]=st.peek();
-26            st.push(i);
-27        }
-28        return pse;
-29    }
-30    public int sumSubarrayMins(int[] arr) {
-31        int mod=1000000007;
-32        int[] pse=findPse(arr);
-33        int[] nse=findNse(arr);
-34        long tot=0;
-35        for(int i=0;i<arr.length;i++){
-36            int ps=i-pse[i];
-37            int ns=nse[i]-i;
-38            tot=(tot+1L*arr[i]*ps*ns)%mod;
-39        }
-40        return (int)tot;
-41    }
-42}
+14        st.clear();
+15        for (int i = n - 1; i >= 0; i--) {
+16            while (!st.isEmpty() && nums[st.peek()]<=nums[i]) st.pop();
+17            nge[i] = st.isEmpty() ? n : st.peek();
+18            st.push(i);
+19        }
+20        st.clear();
+21        for (int i = 0; i < n; i++) {
+22            while (!st.isEmpty() && nums[st.peek()]>nums[i]) st.pop();
+23            pse[i] = st.isEmpty() ? -1 : st.peek();
+24            st.push(i);
+25        }
+26        st.clear();
+27        for (int i = n - 1; i >= 0; i--) {
+28            while (!st.isEmpty() && nums[st.peek()]>=nums[i]) st.pop();
+29            nse[i] = st.isEmpty() ? n : st.peek();
+30            st.push(i);
+31        }
+32        long maxSum = 0, minSum = 0;
+33        for (int i = 0; i < n; i++) {
+34            maxSum += (long)nums[i]*(i - pge[i])*(nge[i] - i);
+35            minSum += (long)nums[i]*(i - pse[i])*(nse[i] - i);
+36        }
+37        return maxSum-minSum;
+38    }
+39}
