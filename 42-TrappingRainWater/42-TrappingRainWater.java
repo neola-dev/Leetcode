bb44abd74fdc4a00317972457726a641420d6d65
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-13 11:33:13 +0530

    File updated at 13/06/2026, 11:33:15

--- 42-TrappingRainWater/42-TrappingRainWater.java
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -1,22 +1,20 @@
-// Last updated: 04/06/2026, 11:20:26
-class Solution {
-    public int trap(int[] height) {
-        int n=height.length;
-        int l=0;
-        int r=n-1;
-        int maxLeft=0,maxRight=0,total=0;
-        while(l<=r){
-            if(height[l]<=height[r]){
-                if(height[l]>maxLeft)   maxLeft=height[l];
-                else total+=maxLeft-height[l];
-                l++;
-            }
-            else{
-                if(height[r]>maxRight)  maxRight=height[r];
-                else total+=maxRight-height[r];
-                r--;
-            }
-        }
-        return total;
-    }
-}
+// Last updated: 13/06/2026, 11:33:15
+1class Solution {
+2    public int trap(int[] height) {
+3        Stack<Integer> st=new Stack<>();
+4        int n=height.length;
+5        int tot=0;
+6        for(int i=0;i<n;i++){
+7            while(!st.isEmpty() && height[i]>height[st.peek()]){
+8                int mid=height[st.pop()];
+9                if(st.isEmpty()) break;
+10                int left=height[st.peek()];
+11                int hei=Math.min(height[i],left)-mid;
+12                int wid=i-(st.peek())-1;
+13                tot+=hei*wid;
+14            }
+15            st.push(i);
+16        }
+17        return tot;
+18    }
+19}
