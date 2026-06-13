331efa81d9d96062e7001293df88cc1f71bca0af
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-13 12:03:50 +0530

    File updated at 13/06/2026, 12:03:52

--- 42-TrappingRainWater/42-TrappingRainWater.java
+++ 42-TrappingRainWater/42-TrappingRainWater.java
@@ -1,6 +1,6 @@
-// Last updated: 13/06/2026, 12:01:29
+// Last updated: 13/06/2026, 12:03:52
 1class Solution {
-2    public int[] findNse(int[] arr){
+2     public int[] findNse(int[] arr){
 3        int n=arr.length;
 4        int[] nse=new int[n];
 5        Stack<Integer> st=new Stack<>();
@@ -30,14 +30,14 @@
 29    }
 30    public int sumSubarrayMins(int[] arr) {
 31        int mod=1000000007;
-32        int[] nse=findNse(arr);
-33        int[] pse=findPse(arr);
-34        long total=0;
+32        int[] pse=findPse(arr);
+33        int[] nse=findNse(arr);
+34        long tot=0;
 35        for(int i=0;i<arr.length;i++){
-36            long nsElmt=nse[i]-i;
-37            long psElmt=(i-pse[i]);
-38            total=(total+arr[i]*(nsElmt*psElmt))%mod;
+36            int ps=i-pse[i];
+37            int ns=nse[i]-i;
+38            tot=(tot+1L*arr[i]*ps*ns)%mod;
 39        }
-40        return (int)total;
+40        return (int)tot;
 41    }
 42}
