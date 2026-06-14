a2bb16fb07f38baaf777bbe69cf9fcd667a600f9
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-14 16:09:39 +0530

    File updated at 14/06/2026, 16:09:41

--- 704-BinarySearch/704-BinarySearch.java
+++ 704-BinarySearch/704-BinarySearch.java
@@ -1,29 +1,20 @@
-// Last updated: 14/06/2026, 15:56:10
-1/** 
-2 * Forward declaration of guess API.
-3 * @param  num   your guess
-4 * @return 	     -1 if num is higher than the picked number
-5 *			      1 if num is lower than the picked number
-6 *               otherwise return 0
-7 * int guess(int num);
-8 */
-9
-10public class Solution extends GuessGame {
-11    public int guessNumber(int n) {
-12        int l=1;
-13        int h=n;
-14        while(l<=h){
-15            int m=l+(h-l)/2;
-16            if(guess(m)==0){
-17                return m;
-18            }
-19            else if(guess(m)==-1){
-20                h=m-1;
-21            }
-22            else{
-23                l=m+1;
-24            }
-25        }
-26        return -1;
-27    }
-28}
+// Last updated: 14/06/2026, 16:09:41
+1class Solution {
+2    public int mySqrt(int x) {
+3        long low=1;
+4        long high=x;
+5        long ans=0;
+6        while(low<=high){
+7            long mid=(low+high)/2;
+8            long square=mid*mid;
+9            if(square<=x){
+10                ans=mid;
+11                low=mid+1;
+12            }
+13            else{
+14                high=mid-1;
+15            }
+16        }
+17        return (int)high;
+18    }
+19}
