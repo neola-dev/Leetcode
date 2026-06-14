f0f59ec33486492e560fdd251953a32edcbe0ac0
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-14 18:50:43 +0530

    File updated at 14/06/2026, 18:50:45

--- 162-FindPeakElement/162-FindPeakElement.java
+++ 162-FindPeakElement/162-FindPeakElement.java
@@ -1,18 +1,18 @@
-// Last updated: 04/06/2026, 11:18:26
-class Solution {
-    public int findPeakElement(int[] nums) {
-        int n=nums.length;
-        if(n==1)    return 0;
-        if(nums[0]>nums[1]) return 0;
-        if(nums[n-1]>nums[n-2]) return n-1;
-        int low=1;
-        int high=n-2;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1])  return mid;
-            else if(nums[mid]>nums[mid-1])  low=mid+1;
-            else high=mid-1;
-        }
-        return -1;
-    }
-}
+// Last updated: 14/06/2026, 18:50:45
+1class Solution {
+2    public int findPeakElement(int[] nums) {
+3        int n=nums.length;
+4        if(n==1)    return 0;
+5        if(nums[0]>nums[1]) return 0;
+6        if(nums[n-1]>nums[n-2]) return n-1;
+7        int l=1;
+8        int h=n-2;
+9        while(l<=h){
+10            int m=l+(h-l)/2;
+11            if(nums[m]>nums[m-1] && nums[m]>nums[m+1])  return m;
+12            if(nums[m]>nums[m-1])   l=m+1;
+13            else h=m-1;
+14        }
+15        return -1;
+16    }
+17}
