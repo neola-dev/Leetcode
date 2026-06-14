01f1b4ead85bd5ca2b9cfefd03189b30a11b4c9d
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-14 17:28:53 +0530

    File updated at 14/06/2026, 17:28:55

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.java
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.java
@@ -1,19 +1,24 @@
-// Last updated: 14/06/2026, 17:17:42
+// Last updated: 14/06/2026, 17:28:55
 1class Solution {
-2    public char nextGreatestLetter(char[] letters, char target) {
-3        int l=0;
-4        int h=letters.length-1;
-5        char ans=letters[0];
-6        while(l<=h){
-7            int m=l+(h-l)/2;
-8            if(letters[m]>target){
-9                ans=letters[m];
-10                h=m-1;
-11            }
-12            else{
-13                l=m+1;
-14            }
-15        }   
-16        return ans;
-17    }
-18}
+2    public boolean searchMatrix(int[][] matrix, int target) {
+3        int n=matrix.length;
+4        int m=matrix[0].length;
+5        int low=0;
+6        int high=n*m-1;
+7        while(low<=high){
+8            int mid=(low+high)/2;
+9            int r=mid/m;
+10            int c=mid%m;
+11            if(matrix[r][c]==target){
+12                return true;
+13            }
+14            else if(matrix[r][c]>target){
+15                high=mid-1;
+16            }
+17            else{
+18                low=mid+1;
+19            }
+20        }
+21        return false;
+22    }
+23}
