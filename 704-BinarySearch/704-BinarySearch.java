01f411923b1f5b146ec3bd5d16de1e829173c531
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-14 15:56:08 +0530

    File updated at 14/06/2026, 15:56:10

--- 704-BinarySearch/704-BinarySearch.java
+++ 704-BinarySearch/704-BinarySearch.java
@@ -1,20 +1,29 @@
-// Last updated: 14/06/2026, 15:50:47
-1class Solution {
-2    public int searchInsert(int[] nums, int target) {
-3        int l=0;
-4        int h=nums.length-1;
-5        while(l<=h){
-6            int m=l+(h-l)/2;
-7            if(nums[m]==target){
-8                return m;
-9            }
-10            else if(nums[m]<target){
-11                l=m+1;
-12            }
-13            else{
-14                h=m-1;
-15            }
-16        }
-17        return l;
-18    }
-19}
+// Last updated: 14/06/2026, 15:56:10
+1/** 
+2 * Forward declaration of guess API.
+3 * @param  num   your guess
+4 * @return 	     -1 if num is higher than the picked number
+5 *			      1 if num is lower than the picked number
+6 *               otherwise return 0
+7 * int guess(int num);
+8 */
+9
+10public class Solution extends GuessGame {
+11    public int guessNumber(int n) {
+12        int l=1;
+13        int h=n;
+14        while(l<=h){
+15            int m=l+(h-l)/2;
+16            if(guess(m)==0){
+17                return m;
+18            }
+19            else if(guess(m)==-1){
+20                h=m-1;
+21            }
+22            else{
+23                l=m+1;
+24            }
+25        }
+26        return -1;
+27    }
+28}
