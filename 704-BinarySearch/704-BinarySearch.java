6c38af559c61200ace5aa82481bd1f36066be566
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-14 15:43:48 +0530

    Create 704-BinarySearch

---
+++ 704-BinarySearch/704-BinarySearch.java
@@ -0,0 +1,20 @@
+// Last updated: 14/06/2026, 15:43:50
+1class Solution {
+2    public int search(int[] nums, int target) {
+3        int l=0;
+4        int h=nums.length-1;
+5        while(l<=h){
+6            int m=l+(h-l)/2;
+7            if(nums[m]==target){
+8                return m;
+9            }
+10            else if(nums[m]<target){
+11                l=m+1;
+12            }
+13            else{
+14                h=m-1;
+15            }
+16        }
+17        return -1;
+18    }
+19}
