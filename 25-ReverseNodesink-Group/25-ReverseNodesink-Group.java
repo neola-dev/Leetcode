411f4acb00d17b0a747c350d59e15b4ed966e708
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-17 16:08:24 +0530

    File updated at 17/06/2026, 16:08:26

--- 25-ReverseNodesink-Group/25-ReverseNodesink-Group.java
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.java
@@ -1,27 +1,18 @@
-// Last updated: 17/06/2026, 15:56:43
-1/**
-2 * Definition for singly-linked list.
-3 * public class ListNode {
-4 *     int val;
-5 *     ListNode next;
-6 *     ListNode() {}
-7 *     ListNode(int val) { this.val = val; }
-8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-9 * }
-10 */
-11class Solution {
-12    public ListNode removeNthFromEnd(ListNode head, int n) {
-13        ListNode fast=head;
-14        for(int i=0;i<n;i++){
-15            fast=fast.next;
-16        }
-17        if(fast==null)  return head.next;
-18        ListNode slow=head;
-19        while(fast.next!=null){
-20            slow=slow.next;
-21            fast=fast.next;
-22        }
-23        slow.next=slow.next.next;
-24        return head;
-25    }
-26}
+// Last updated: 17/06/2026, 16:08:26
+1class Solution {
+2    public int findDuplicate(int[] nums) {
+3        int slow=nums[0];
+4        int fast=nums[0];
+5        do{
+6            slow=nums[slow];
+7            fast=nums[nums[fast]];
+8        }
+9        while(slow!=fast);
+10        fast=nums[0];
+11        while(slow!=fast){
+12            slow=nums[slow];
+13            fast=nums[fast];
+14        }
+15        return slow;
+16    }
+17}
