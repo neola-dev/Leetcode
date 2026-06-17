1d827bb1116fe6139a69461a0dfc64d9c96842f7
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-17 13:37:40 +0530

    File updated at 17/06/2026, 13:37:42

--- 61-RotateList/61-RotateList.java
+++ 61-RotateList/61-RotateList.java
@@ -1,32 +1,32 @@
-// Last updated: 04/06/2026, 11:20:01
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode rotateRight(ListNode head, int k) {
-        if(k==0 || head==null)  return head;
-        int n=1;
-        ListNode temp=head;
-        while(temp.next!=null){
-            n++;
-            temp=temp.next;
-        }
-        temp.next=head;
-        k=k%n;
-        ListNode newTail=head;
-        int steps=n-k;
-        for(int i=1;i<steps;i++){
-            newTail=newTail.next;
-        }
-        ListNode newHead=newTail.next;
-        newTail.next=null;
-        return newHead;
-    }
-}
+// Last updated: 17/06/2026, 13:37:42
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode rotateRight(ListNode head, int k) {
+13        if(k==0 || head==null)  return head;
+14        int n=1;
+15        ListNode temp=head;
+16        while(temp.next!=null){
+17            n++;
+18            temp=temp.next;
+19        }
+20        temp.next=head;
+21        k=k%n;
+22        ListNode newTail=head;
+23        int steps=n-k;
+24        for(int i=1;i<steps;i++){
+25            newTail=newTail.next;
+26        }
+27        ListNode newHead=newTail.next;
+28        newTail.next=null;
+29        return newHead;
+30    }
+31}
