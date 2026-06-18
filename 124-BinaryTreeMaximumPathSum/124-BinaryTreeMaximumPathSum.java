977a6bcdfa2271110a2168007517fd204946aac7
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-18 11:39:37 +0530

    File updated at 18/06/2026, 11:39:39

--- 124-BinaryTreeMaximumPathSum/124-BinaryTreeMaximumPathSum.java
+++ 124-BinaryTreeMaximumPathSum/124-BinaryTreeMaximumPathSum.java
@@ -1,32 +1,31 @@
-// Last updated: 04/06/2026, 11:18:58
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    int maxSum=Integer.MIN_VALUE;
-    public int dfs(TreeNode root){
-        if(root==null){
-            return 0;
-        }
-        int leftPathMax=Math.max(0,dfs(root.left));
-        int rightPathMax=Math.max(0,dfs(root.right));
-        maxSum=Math.max(maxSum,leftPathMax+rightPathMax+root.val);
-        return Math.max(leftPathMax,rightPathMax)+root.val;
-    }
-    public int maxPathSum(TreeNode root) {
-        dfs(root);
-        return maxSum;
-    }
-}
+// Last updated: 18/06/2026, 11:39:39
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    int maxSum=Integer.MIN_VALUE;
+18    public int dfs(TreeNode root){
+19        if(root==null)  return 0;
+20        int left=Math.max(0,dfs(root.left));
+21        int right=Math.max(0,dfs(root.right));
+22        maxSum=Math.max(maxSum,left+right+root.val);
+23        return Math.max(left,right)+root.val;
+24    }
+25    public int maxPathSum(TreeNode root) {
+26        if(root==null)  return 0;
+27        dfs(root);
+28        return maxSum;
+29    }
+30}
