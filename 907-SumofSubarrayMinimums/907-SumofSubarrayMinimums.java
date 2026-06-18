aa3eaf125065a738bb96847050d62f57c3b623c0
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-18 10:30:02 +0530

    File updated at 18/06/2026, 10:30:05

--- 907-SumofSubarrayMinimums/907-SumofSubarrayMinimums.java
+++ 907-SumofSubarrayMinimums/907-SumofSubarrayMinimums.java
@@ -1,4 +1,4 @@
-// Last updated: 18/06/2026, 10:08:36
+// Last updated: 18/06/2026, 10:30:05
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -15,8 +15,13 @@
 14 * }
 15 */
 16class Solution {
-17    public int maxDepth(TreeNode root) {
-18        if(root==null)  return 0;
-19        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+17    public boolean isSymmetric(TreeNode root) {
+18        if(root==null)  return false;
+19        return find(root.left,root.right);
 20    }
-21}
+21    public boolean find(TreeNode p,TreeNode q){
+22        if(p==null && q==null)  return true;
+23        if(p==null || q==null || p.val!=q.val)  return false;
+24        return find(p.left,q.right) && find(p.right,q.left);
+25    }
+26}
