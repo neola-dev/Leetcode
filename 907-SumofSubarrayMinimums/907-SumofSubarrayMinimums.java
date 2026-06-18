441f5148c65d212265adc9e7e28788deea907e81
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-18 09:50:35 +0530

    File updated at 18/06/2026, 09:50:38

--- 907-SumofSubarrayMinimums/907-SumofSubarrayMinimums.java
+++ 907-SumofSubarrayMinimums/907-SumofSubarrayMinimums.java
@@ -1,4 +1,4 @@
-// Last updated: 18/06/2026, 09:46:54
+// Last updated: 18/06/2026, 09:50:38
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -15,15 +15,15 @@
 14 * }
 15 */
 16class Solution {
-17    public List<Integer> inorderTraversal(TreeNode root) {
+17    public List<Integer> preorderTraversal(TreeNode root) {
 18        List<Integer> res=new ArrayList<>();
 19        dfs(root,res);
 20        return res;
 21    }
 22    public void dfs(TreeNode root,List<Integer> res){
 23        if(root==null)  return;
-24        dfs(root.left,res);
-25        res.add(root.val);
+24        res.add(root.val);
+25        dfs(root.left,res);
 26        dfs(root.right,res);
 27    }
 28}
