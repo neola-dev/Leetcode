b467e2af9c2195df845ecd0f656a591b452fdbcb
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-19 11:04:51 +0530

    File updated at 19/06/2026, 11:04:53

--- 55-JumpGame/55-JumpGame.java
+++ 55-JumpGame/55-JumpGame.java
@@ -1,11 +1,11 @@
-// Last updated: 04/06/2026, 11:20:09
-class Solution {
-    public boolean canJump(int[] nums) {
-        int maxInd=0;
-        for(int i=0;i<nums.length;i++){
-            if(i>maxInd)    return false;
-            maxInd=Math.max(maxInd,i+nums[i]);
-        }
-        return true;
-    }
-}
+// Last updated: 19/06/2026, 11:04:53
+1class Solution {
+2    public boolean canJump(int[] nums) {
+3        int maxInd=0;
+4        for(int i=0;i<nums.length;i++){
+5            if(i>maxInd)    return false;
+6            maxInd=Math.max(maxInd,i+nums[i]);
+7        }
+8        return true;
+9    }
+10}
