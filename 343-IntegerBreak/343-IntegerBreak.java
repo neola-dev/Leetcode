5e676722dcbfcf94eb97fd017e9eb3a2a4ef0496
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-23 10:15:00 +0530

    File updated at 23/06/2026, 10:15:00

--- 343-IntegerBreak/343-IntegerBreak.java
+++ 343-IntegerBreak/343-IntegerBreak.java
@@ -1,13 +1,33 @@
-// Last updated: 23/06/2026, 10:09:42
-1class Solution {
-2    public int integerBreak(int n) {
-3        int[] dp=new int[n+1];
-4        dp[2]=1;
-5        for(int i=3;i<=n;i++){
-6            for(int j=1;j<i;j++){
-7                dp[i]=Math.max(dp[i],Math.max(j*(i-j),j*dp[i-j]));
-8            }
-9        }
-10        return dp[n];
-11    }
-12}
+// Last updated: 23/06/2026, 10:15:00
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    List<Integer> ans=new ArrayList<>();
+18    public List<Integer> rightSideView(TreeNode root) {
+19        dfs(0,root);
+20        return ans;
+21    }
+22    public void dfs(int depth,TreeNode root){
+23        if(root==null){
+24            return;
+25        }
+26        if(depth==ans.size()){
+27            ans.add(root.val);
+28        }
+29        dfs(depth+1,root.right);
+30        dfs(depth+1,root.left);
+31    }
+32}
