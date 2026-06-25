233cb6929cfa155e488f575cd698e2fcdd0faefd
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-25 09:48:10 +0530

    File updated at 25/06/2026, 09:48:11

--- 1155-NumberofDiceRollsWithTargetSum/1155-NumberofDiceRollsWithTargetSum.java
+++ 1155-NumberofDiceRollsWithTargetSum/1155-NumberofDiceRollsWithTargetSum.java
@@ -1,4 +1,4 @@
-// Last updated: 25/06/2026, 09:47:42
+// Last updated: 25/06/2026, 09:48:11
 1/**
 2 * Definition for a binary tree node.
 3 * public class TreeNode {
@@ -12,13 +12,16 @@
 11class Solution {
 12    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 13        while(root!=null){
-14            if(p.val<root.val && q.val<root.val)
+14            if(p.val<root.val && q.val<root.val){
 15                root=root.left;
-16            else if(p.val>root.val && q.val>root.val)
-17                root=root.right;
-18            else
-19                return root;
-20        }
-21        return null;
-22    }
-23}
+16            }
+17            else if(p.val>root.val && q.val>root.val){
+18                root=root.right;
+19            }
+20            else{
+21                return root;
+22            }
+23        }
+24        return null;
+25    }
+26}
