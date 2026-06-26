676787dc8059fc83622142f1768bf7a8dd089441
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-26 13:21:04 +0530

    File updated at 26/06/2026, 13:21:06

--- 22-GenerateParentheses/22-GenerateParentheses.java
+++ 22-GenerateParentheses/22-GenerateParentheses.java
@@ -1,25 +1,24 @@
-// Last updated: 04/06/2026, 11:20:51
-class Solution {
-    public void helper(int open,int closed,int n,List<String> res,StringBuilder sb){
-        if(open==n && closed==n){
-            res.add(sb.toString());
-            return;
-        }
-        
-        if(open<n){
-            sb.append("(");
-            helper(open+1,closed,n,res,sb);
-            sb.deleteCharAt(sb.length()-1);
-        }
-        if(closed<open){
-            sb.append(")");
-            helper(open,closed+1,n,res,sb);
-            sb.deleteCharAt(sb.length()-1);
-        }
-    }
-    public List<String> generateParenthesis(int n) {
-        List<String> res=new ArrayList<>();
-        helper(0,0,n,res,new StringBuilder());
-        return res;
-    }
-}
+// Last updated: 26/06/2026, 13:21:06
+1class Solution {
+2    public void recur(int open,int close,List<String> res,int n,StringBuilder sb){
+3        if(open==n && close==n){
+4            res.add(sb.toString());
+5            return;
+6        }
+7        if(open<n){
+8            sb.append("(");
+9            recur(open+1,close,res,n,sb);
+10            sb.deleteCharAt(sb.length()-1);
+11        }
+12        if(close<open){
+13            sb.append(")");
+14            recur(open,close+1,res,n,sb);
+15            sb.deleteCharAt(sb.length()-1);
+16        }
+17    }
+18    public List<String> generateParenthesis(int n) {
+19        List<String> res=new ArrayList<>();
+20        recur(0,0,res,n,new StringBuilder());
+21        return res;
+22    }
+23}
