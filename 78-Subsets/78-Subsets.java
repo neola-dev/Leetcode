07de45ceaa21f9d342ca7e0768e54c74208a0340
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-26 14:33:40 +0530

    File updated at 26/06/2026, 14:33:42

--- 78-Subsets/78-Subsets.java
+++ 78-Subsets/78-Subsets.java
@@ -1,22 +1,23 @@
-// Last updated: 26/06/2026, 14:09:19
+// Last updated: 26/06/2026, 14:33:42
 1class Solution {
-2    public void recur(int ind,int[] arr,int cnt,int k,int n,List<List<Integer>> res,List<Integer> sub){
+2    public void recur(int ind,int[] arr,int n,int cnt,int k,List<List<Integer>> res,List<Integer> sub){
 3        if(cnt==k){
-4            if(n==0){
-5                res.add(new ArrayList<>(sub));
-6            }
-7            return;
-8        }
-9        if(ind>=arr.length || n<0)  return;
-10        sub.add(arr[ind]);
-11        recur(ind+1,arr,cnt+1,k,n-arr[ind],res,sub);
-12        sub.remove(sub.size()-1);
-13        recur(ind+1,arr,cnt,k,n,res,sub);
-14    }
-15    public List<List<Integer>> combinationSum3(int k, int n) {
-16        List<List<Integer>> res=new ArrayList<>();
-17        int[] arr={1,2,3,4,5,6,7,8,9};
-18        recur(0,arr,0,k,n,res,new ArrayList<>());
-19        return res;
-20    }
-21}
+4            res.add(new ArrayList<>(sub));
+5            return;
+6        }
+7        if(ind==n)  return;
+8        sub.add(arr[ind]);
+9        recur(ind+1,arr,n,cnt+1,k,res,sub);
+10        sub.remove(sub.size()-1);
+11        recur(ind+1,arr,n,cnt,k,res,sub);
+12    }
+13    public List<List<Integer>> combine(int n, int k) {
+14        List<List<Integer>> res=new ArrayList<>();
+15        int[] arr=new int[n];
+16        for(int i=0;i<n;i++){
+17            arr[i]=i+1;
+18        }
+19        recur(0,arr,n,0,k,res,new ArrayList<>());
+20        return res;
+21    }
+22}
