8656ca04089007a6eff1cbcf895e80ae79634a5a
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-26 13:48:56 +0530

    File updated at 26/06/2026, 13:48:58

--- 78-Subsets/78-Subsets.java
+++ 78-Subsets/78-Subsets.java
@@ -1,22 +1,24 @@
-// Last updated: 26/06/2026, 13:41:49
+// Last updated: 26/06/2026, 13:48:58
 1class Solution {
-2    public void recur(int ind,int[] candidates,int sum,int target,List<Integer> sub,List<List<Integer>> res){
-3        if(ind==candidates.length){
-4            if(sum==target){
-5                res.add(new ArrayList<>(sub));
-6            }
-7            return;
-8        }   
-9        if(sum+candidates[ind]<=target){ 
-10            sub.add(candidates[ind]);
-11            recur(ind,candidates,sum+candidates[ind],target,sub,res);
+2    public static void helper(int ind,int[] arr,int target,int n,List<List<Integer>> res,List<Integer> sub){
+3        if(target==0){
+4            res.add(new ArrayList<>(sub));
+5            return;
+6        }
+7        for(int i=ind;i<n;i++){
+8            if(i>ind && arr[i]==arr[i-1])   continue;
+9            if(arr[i]>target)   break;
+10            sub.add(arr[i]);
+11            helper(i+1,arr,target-arr[i],n,res,sub);
 12            sub.remove(sub.size()-1);
-13        }    
-14        recur(ind+1,candidates,sum,target,sub,res);
-15    }
-16    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-17        List<List<Integer>> res=new ArrayList<>();
-18        recur(0,candidates,0,target,new ArrayList<>(),res);
-19        return res;
-20    }
-21}
+13        }
+14    }
+15    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+16        ArrayList<List<Integer>> res=new ArrayList<>();
+17        List<Integer> sub=new ArrayList<>();
+18        int n=candidates.length;
+19        Arrays.sort(candidates);
+20        helper(0,candidates,target,n,res,sub);
+21        return res;
+22    }
+23}
