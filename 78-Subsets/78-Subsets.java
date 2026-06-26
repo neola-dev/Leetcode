f4a1428aa38b74a661d2bda93d85266c054dfaf2
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-26 13:09:46 +0530

    File updated at 26/06/2026, 13:09:48

--- 78-Subsets/78-Subsets.java
+++ 78-Subsets/78-Subsets.java
@@ -1,18 +1,19 @@
-// Last updated: 26/06/2026, 13:09:14
+// Last updated: 26/06/2026, 13:09:48
 1class Solution {
-2    public static void find(int ind,int[] nums,List<List<Integer>> res,List<Integer> ds){
-3        res.add(new ArrayList<>(ds));
-4        for(int i=ind;i<nums.length;i++){
-5            if(i!=ind && nums[i]==nums[i-1]) continue;
-6            ds.add(nums[i]);
-7            find(i+1,nums,res,ds);
-8            ds.remove(ds.size()-1);
-9        }
-10    }
-11    public List<List<Integer>> subsetsWithDup(int[] nums) {
-12        List<List<Integer>> res=new ArrayList<>();
-13        Arrays.sort(nums);
-14        find(0,nums,res,new ArrayList<>());
-15        return res;
-16    }
-17}
+2    public static void recur(int ind,int[] nums,List<List<Integer>> res,List<Integer> sub,int n){
+3        if(ind==n){
+4            if(!res.contains(sub)) res.add(new ArrayList<>(sub));
+5            return;
+6        }
+7        sub.add(nums[ind]);
+8        recur(ind+1,nums,res,sub,n);
+9        sub.remove(sub.size()-1);
+10        recur(ind+1,nums,res,sub,n);
+11    }
+12    public List<List<Integer>> subsetsWithDup(int[] nums) {
+13        List<List<Integer>> res=new ArrayList<>();
+14        Arrays.sort(nums);
+15        recur(0,nums,res,new ArrayList<>(),nums.length);
+16        return res;
+17    }
+18}
