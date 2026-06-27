1d55064f151297c0bdf076c253fddc040ebc9b8b
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-27 12:45:18 +0530

    File updated at 27/06/2026, 12:45:20

--- 46-Permutations/46-Permutations.java
+++ 46-Permutations/46-Permutations.java
@@ -1,30 +1,31 @@
-// Last updated: 27/06/2026, 12:01:30
+// Last updated: 27/06/2026, 12:45:20
 1class Solution {
-2    public void recur(int ind,String s,List<String> res,int n,StringBuilder sb){
-3        if(sb.length()==n){
-4            res.add(sb.toString());
-5            return;
-6        }
-7        char ch=s.charAt(ind);
-8        boolean isDig=(int)ch>=48 && (int)ch<=57;
-9        if(isDig){
-10            sb.append(ch);
-11            recur(ind+1,s,res,n,sb);
-12            sb.deleteCharAt(sb.length()-1);
-13        }
-14        else{
-15            sb.append(Character.toLowerCase(ch));
-16            recur(ind+1,s,res,n,sb);
-17            sb.deleteCharAt(sb.length()-1);
-18            sb.append(Character.toUpperCase(ch));
-19            recur(ind+1,s,res,n,sb);
-20            sb.deleteCharAt(sb.length()-1);
-21        }
-22    }
-23    public List<String> letterCasePermutation(String s) {
-24        List<String> res=new ArrayList<>();
-25        int n=s.length();
-26        recur(0,s,res,n,new StringBuilder());
-27        return res;
-28    }
-29}
+2    public void recur(String s,int ind,List<String> res,int n,StringBuilder sb,int parts){
+3        if(parts==4){
+4            if(ind==n){
+5                res.add(sb.toString());
+6            }
+7            return;
+8        }
+9        for(int len=1;len<=3;len++){
+10            if(ind+len>n)   break;
+11            String sub=s.substring(ind,ind+len);
+12            if(sub.length()>1 && sub.charAt(0)=='0') break;
+13            int num=Integer.parseInt(sub);
+14            if(num>255){
+15                break;
+16            }
+17            int size=sb.length();
+18            if(parts>0) sb.append('.'); 
+19            sb.append(sub);
+20            recur(s,ind+len,res,n,sb,parts+1);
+21            sb.setLength(size);
+22        }
+23    }
+24    public List<String> restoreIpAddresses(String s) {
+25        List<String> res=new ArrayList<>();
+26        int n=s.length();
+27        recur(s,0,res,n,new StringBuilder(),0);
+28        return res;
+29    }
+30}
