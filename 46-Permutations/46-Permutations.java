3df0735c726d19619d4a3dc0fd36d6e54121971b
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-27 12:01:28 +0530

    File updated at 27/06/2026, 12:01:30

--- 46-Permutations/46-Permutations.java
+++ 46-Permutations/46-Permutations.java
@@ -1,27 +1,30 @@
-// Last updated: 27/06/2026, 11:39:00
+// Last updated: 27/06/2026, 12:01:30
 1class Solution {
-2    public void recur(char[] arr,List<String> res,int n,boolean[] vis,StringBuilder sb){
+2    public void recur(int ind,String s,List<String> res,int n,StringBuilder sb){
 3        if(sb.length()==n){
 4            res.add(sb.toString());
 5            return;
 6        }
-7        for(int i=0;i<arr.length;i++){
-8            if(vis[i])  continue;
-9            sb.append(arr[i]);
-10            vis[i]=true;
-11            recur(arr,res,n,vis,sb);
-12            vis[i]=false;
-13            sb.deleteCharAt(sb.length()-1);
-14        }
-15    }
-16    public String getPermutation(int n, int k) {
-17        char[] arr=new char[n];
-18        for(int i=0;i<n;i++){
-19            arr[i]=(char)((i+1)+'0');
-20        }
-21        List<String> res=new ArrayList<>();
-22        boolean[] vis=new boolean[n];
-23        recur(arr,res,n,vis,new StringBuilder());
-24        return res.get(k-1);
-25    }
-26}
+7        char ch=s.charAt(ind);
+8        boolean isDig=(int)ch>=48 && (int)ch<=57;
+9        if(isDig){
+10            sb.append(ch);
+11            recur(ind+1,s,res,n,sb);
+12            sb.deleteCharAt(sb.length()-1);
+13        }
+14        else{
+15            sb.append(Character.toLowerCase(ch));
+16            recur(ind+1,s,res,n,sb);
+17            sb.deleteCharAt(sb.length()-1);
+18            sb.append(Character.toUpperCase(ch));
+19            recur(ind+1,s,res,n,sb);
+20            sb.deleteCharAt(sb.length()-1);
+21        }
+22    }
+23    public List<String> letterCasePermutation(String s) {
+24        List<String> res=new ArrayList<>();
+25        int n=s.length();
+26        recur(0,s,res,n,new StringBuilder());
+27        return res;
+28    }
+29}
