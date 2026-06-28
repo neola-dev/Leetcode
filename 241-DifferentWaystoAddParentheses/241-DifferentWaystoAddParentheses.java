d7c1fab88eeb66d9d4253d7adf1f91ba949c17b0
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-28 12:56:02 +0530

    File updated at 28/06/2026, 12:56:05

--- 241-DifferentWaystoAddParentheses/241-DifferentWaystoAddParentheses.java
+++ 241-DifferentWaystoAddParentheses/241-DifferentWaystoAddParentheses.java
@@ -1,27 +1,30 @@
-// Last updated: 28/06/2026, 12:18:59
+// Last updated: 28/06/2026, 12:56:05
 1class Solution {
-2    public boolean recur(int i,int j,int n,int m,char[][] board,String word,int k){
-3        if(k==word.length())    return true;
-4        if(i<0 || j<0 || i>=n || j>=m || board[i][j]!=word.charAt(k))   return false;
-5        char ch=board[i][j];
-6        board[i][j]='#';
-7        boolean up=recur(i-1,j,n,m,board,word,k+1);
-8        boolean down=recur(i+1,j,n,m,board,word,k+1);
-9        boolean left=recur(i,j-1,n,m,board,word,k+1);
-10        boolean right=recur(i,j+1,n,m,board,word,k+1);
-11        board[i][j]=ch;
-12        return up || down || left || right;
+2    public int recur(int i,int j,int n,int m,boolean[][]vis,int[][] grid){
+3        if(i<0 || j<0 || i>=n || j>=m)  return 0;
+4        if(grid[i][j]==0 || vis[i][j])  return 0;
+5        int curr=grid[i][j];
+6        vis[i][j]=true;
+7        int up=recur(i-1,j,n,m,vis,grid);
+8        int down=recur(i+1,j,n,m,vis,grid);
+9        int left=recur(i,j-1,n,m,vis,grid);
+10        int right=recur(i,j+1,n,m,vis,grid);
+11        vis[i][j]=false;
+12        return curr+Math.max(Math.max(up,down),Math.max(left,right));
 13    }
-14    public boolean exist(char[][] board, String word) {
-15        int n=board.length;
-16        int m=board[0].length;
-17        for(int i=0;i<n;i++){
-18            for(int j=0;j<m;j++){
-19                if(board[i][j]==word.charAt(0)){
-20                    if(recur(i,j,n,m,board,word,0)) return true;
-21                }
-22            }
-23        }
-24        return false;
-25    }
-26}
+14    public int getMaximumGold(int[][] grid) {
+15        int n=grid.length;
+16        int m=grid[0].length;
+17        int max=0;
+18        boolean[][] vis=new boolean[n][m];
+19        for(int i=0;i<n;i++){
+20            for(int j=0;j<m;j++){
+21                if(grid[i][j]!=0){
+22                    int gold=recur(i,j,n,m,vis,grid);
+23                    max=Math.max(max,gold);
+24                }
+25            }
+26        }
+27        return max;
+28    }
+29}
