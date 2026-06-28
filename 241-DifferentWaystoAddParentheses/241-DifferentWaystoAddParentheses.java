24249ede786fed670012f9797600ee28e07c4d08
Neola Kellsie P <neolapeter895@gmail.com>
2026-06-28 12:18:56 +0530

    File updated at 28/06/2026, 12:18:59

--- 241-DifferentWaystoAddParentheses/241-DifferentWaystoAddParentheses.java
+++ 241-DifferentWaystoAddParentheses/241-DifferentWaystoAddParentheses.java
@@ -1,36 +1,27 @@
-// Last updated: 28/06/2026, 11:48:34
+// Last updated: 28/06/2026, 12:18:59
 1class Solution {
-2    public void recur(int ind,long curr,String num,long LastOp,int target,StringBuilder sb,List<String> res){
-3        if(ind==num.length()){
-4            if(curr==target) res.add(sb.toString());
-5            return;
-6        }
-7        for(int len=1;ind+len<=num.length();len++){
-8            String sub=num.substring(ind,ind+len);
-9            if(sub.length()>1 && sub.charAt(0)=='0')    break;
-10            Long value=Long.parseLong(sub);
-11            int subLen=sb.length();
-12            if(ind==0){
-13                sb.append(sub);
-14                recur(ind+len,value,num,value,target,sb,res);
-15                sb.setLength(subLen);
-16            }
-17            else{
-18                sb.append('+').append(sub);
-19                recur(ind+len,curr+value,num,value,target,sb,res);
-20                sb.setLength(subLen);
-21                sb.append('-').append(sub);
-22                recur(ind+len,curr-value,num,-value,target,sb,res);
-23                sb.setLength(subLen);
-24                sb.append('*').append(sub);
-25                recur(ind+len,curr - LastOp + LastOp * value,num,LastOp*value,target,sb,res);
-26                sb.setLength(subLen);
-27            }
-28        }
-29    }
-30    public List<String> addOperators(String num, int target) {
-31        List<String> res=new ArrayList<>();
-32        recur(0,0,num,0,target,new StringBuilder(),res);
-33        return res;
-34    }
-35}
+2    public boolean recur(int i,int j,int n,int m,char[][] board,String word,int k){
+3        if(k==word.length())    return true;
+4        if(i<0 || j<0 || i>=n || j>=m || board[i][j]!=word.charAt(k))   return false;
+5        char ch=board[i][j];
+6        board[i][j]='#';
+7        boolean up=recur(i-1,j,n,m,board,word,k+1);
+8        boolean down=recur(i+1,j,n,m,board,word,k+1);
+9        boolean left=recur(i,j-1,n,m,board,word,k+1);
+10        boolean right=recur(i,j+1,n,m,board,word,k+1);
+11        board[i][j]=ch;
+12        return up || down || left || right;
+13    }
+14    public boolean exist(char[][] board, String word) {
+15        int n=board.length;
+16        int m=board[0].length;
+17        for(int i=0;i<n;i++){
+18            for(int j=0;j<m;j++){
+19                if(board[i][j]==word.charAt(0)){
+20                    if(recur(i,j,n,m,board,word,0)) return true;
+21                }
+22            }
+23        }
+24        return false;
+25    }
+26}
