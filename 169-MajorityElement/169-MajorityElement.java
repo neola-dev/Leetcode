fba16429e60803a49626edd8e35cfe4ef851d90f
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-07 11:42:01 +0530

    File updated at 07/07/2026, 11:42:01

--- 169-MajorityElement/169-MajorityElement.java
+++ 169-MajorityElement/169-MajorityElement.java
@@ -1,12 +1,24 @@
-// Last updated: 07/07/2026, 11:20:08
+// Last updated: 07/07/2026, 11:42:01
 1class Solution {
-2    public int hIndex(int[] citations) {
-3        Arrays.sort(citations);
-4        int n=citations.length;
-5        for(int i=0;i<n;i++){
-6            int h=n-i;
-7            if(citations[i]>=h) return h;
-8        }
-9        return 0;
-10    }
-11}
+2    public int romanToInt(String s) {
+3        HashMap<Character,Integer> map=new HashMap<>();
+4        map.put('I',1);
+5        map.put('V',5);
+6        map.put('X',10);
+7        map.put('L',50);
+8        map.put('C',100);
+9        map.put('D',500);
+10        map.put('M',1000);
+11        int sum=0;
+12        for(int i=0;i<s.length();i++){
+13            int curr=map.get(s.charAt(i));
+14            if(i+1<s.length() && curr<map.get(s.charAt(i+1))){
+15                sum-=curr;
+16            }
+17            else{
+18                sum+=curr;
+19            }
+20        }
+21        return sum;
+22    }
+23}
