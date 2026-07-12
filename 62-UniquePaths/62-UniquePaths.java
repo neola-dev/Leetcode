a2b1654668704f4e1edac919c8c843d97ae5e461
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-12 11:35:42 +0530

    File updated at 12/07/2026, 11:35:44

--- 62-UniquePaths/62-UniquePaths.java
+++ 62-UniquePaths/62-UniquePaths.java
@@ -1,14 +1,14 @@
-// Last updated: 04/06/2026, 11:19:59
-class Solution {
-    public int uniquePaths(int m, int n) {
-        int[][] dp=new int[m][n];
-        for(int i=0;i<m;i++)    dp[i][0]=1;
-        for(int j=0;j<n;j++)    dp[0][j]=1;
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
-            }
-        }
-        return dp[m-1][n-1];
-    }
-}
+// Last updated: 12/07/2026, 11:35:44
+1class Solution {
+2    public int uniquePaths(int m, int n) {
+3        int[][] dp=new int[m][n];
+4        for(int i=0;i<m;i++)    dp[i][0]=1;
+5        for(int j=0;j<n;j++)    dp[0][j]=1;
+6        for(int i=1;i<m;i++){
+7            for(int j=1;j<n;j++){
+8                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+9            }
+10        }
+11        return dp[m-1][n-1];
+12    }
+13}
