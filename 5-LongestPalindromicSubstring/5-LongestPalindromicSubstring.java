f5cbe57b3bba380f5e1e037c475fc510b004f7c1
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-16 21:45:56 +0530

    File updated at 16/07/2026, 21:45:57

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
@@ -1,25 +1,24 @@
-// Last updated: 16/07/2026, 20:49:05
+// Last updated: 16/07/2026, 21:45:57
 1class Solution {
-2    public String expandAroundCenter(String s,int i,int j){
-3        while(i>=0 && j<s.length()){
-4            if(s.charAt(i)==s.charAt(j)){
-5                i--;
-6                j++;
-7            }
-8            else{
-9                break;
-10            }
+2    public void recur(int open,int close,List<String> res,StringBuilder sb,int n){
+3        if(open==n && close==n){
+4            res.add(sb.toString());
+5            return;
+6        }
+7        if(open<n){
+8            sb.append("(");
+9            recur(open+1,close,res,sb,n);
+10            sb.deleteCharAt(sb.length()-1);
 11        }
-12        return s.substring(i+1,j);
-13    }
-14    public String longestPalindrome(String s) {
-15        String longest="";
-16        for(int i=0;i<s.length();i++){
-17            String odd=expandAroundCenter(s,i,i);
-18            if(odd.length()>longest.length()) longest=odd;
-19            String even=expandAroundCenter(s,i,i+1);
-20            if(even.length()>longest.length()) longest=even;
-21        }
-22        return longest;
-23    }
-24}
+12        if(close<open){
+13            sb.append(")");
+14            recur(open,close+1,res,sb,n);
+15            sb.deleteCharAt(sb.length()-1);
+16        }
+17    }
+18    public List<String> generateParenthesis(int n) {
+19        List<String> res=new ArrayList<>();
+20        recur(0,0,res,new StringBuilder(),n);
+21        return res;
+22    }
+23}
