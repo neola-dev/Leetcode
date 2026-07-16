f0329c9bc2fcb13f1580e276c7e9eba02edd20c1
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-16 20:49:04 +0530

    File updated at 16/07/2026, 20:49:05

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.java
@@ -1,27 +1,25 @@
-// Last updated: 04/06/2026, 11:21:11
-class Solution {
-    public String expandAroundCenter(String s,int i,int j){
-        int length=s.length();
-        while(i>=0 && j<length){
-            if(s.charAt(i)==s.charAt(j)){
-                i--;
-                j++;
-            }
-            else{
-                break;
-            }
-        }
-        return s.substring(i+1,j);
-    }
-    public String longestPalindrome(String s) {
-        int n=s.length();
-        String longest="";
-        for(int i=0;i<n;i++){
-            String odd=expandAroundCenter(s,i,i);
-            if(odd.length()>longest.length())   longest=odd;
-            String even=expandAroundCenter(s,i,i+1);
-            if(even.length()>longest.length())  longest=even;
-        }
-        return longest;
-    }
-}
+// Last updated: 16/07/2026, 20:49:05
+1class Solution {
+2    public String expandAroundCenter(String s,int i,int j){
+3        while(i>=0 && j<s.length()){
+4            if(s.charAt(i)==s.charAt(j)){
+5                i--;
+6                j++;
+7            }
+8            else{
+9                break;
+10            }
+11        }
+12        return s.substring(i+1,j);
+13    }
+14    public String longestPalindrome(String s) {
+15        String longest="";
+16        for(int i=0;i<s.length();i++){
+17            String odd=expandAroundCenter(s,i,i);
+18            if(odd.length()>longest.length()) longest=odd;
+19            String even=expandAroundCenter(s,i,i+1);
+20            if(even.length()>longest.length()) longest=even;
+21        }
+22        return longest;
+23    }
+24}
