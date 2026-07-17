e9ff869e3791328b9aaeff7a0d0046bbb6034846
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-17 08:58:40 +0530

    File updated at 17/07/2026, 08:58:41

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,14 +1,14 @@
-// Last updated: 04/06/2026, 11:19:51
-class Solution {
-    public int climbStairs(int n) {
-        if(n<=2)    return n;
-        int prev2=1;
-        int prev1=2;
-        for(int i=3;i<=n;i++){
-            int curr=prev1+prev2;
-            prev2=prev1;
-            prev1=curr;
-        }
-        return prev1;
-    }
-}
+// Last updated: 17/07/2026, 08:58:41
+1class Solution {
+2    public int climbStairs(int n) {
+3        if(n<=2) return n;
+4        int[] dp=new int[n+1];
+5        dp[0]=0;
+6        dp[1]=1;
+7        dp[2]=2;
+8        for(int i=3;i<=n;i++){
+9            dp[i]=dp[i-1]+dp[i-2];
+10        }
+11        return dp[n];
+12    }
+13}
