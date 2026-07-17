b66bfab03ff6766199f5fb8e5c2866b58be0ef11
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-17 12:35:34 +0530

    File updated at 17/07/2026, 12:35:34

--- 62-UniquePaths/62-UniquePaths.java
+++ 62-UniquePaths/62-UniquePaths.java
@@ -1,24 +1,14 @@
-// Last updated: 12/07/2026, 11:48:29
+// Last updated: 17/07/2026, 12:35:34
 1class Solution {
-2    public int uniquePathsWithObstacles(int[][] obs) {
-3        int n=obs.length;
-4        int m=obs[0].length;
-5        int[][] dp=new int[n][m];
-6        if(obs[0][0]==1) return 0;
-7        dp[0][0]=1;
-8        for(int i=1;i<n;i++){
-9            if(obs[i][0]==0)  dp[i][0]=dp[i-1][0];
+2    public int uniquePaths(int m, int n) {
+3        int[][] dp=new int[m][n];
+4        for(int i=0;i<m;i++) dp[i][0]=1;
+5        for(int j=0;j<n;j++) dp[0][j]=1;
+6        for(int i=1;i<m;i++){
+7            for(int j=1;j<n;j++){
+8                dp[i][j]=dp[i][j-1]+dp[i-1][j];
+9            }
 10        }
-11        for(int j=1;j<m;j++){
-12            if(obs[0][j]==0)  dp[0][j]=dp[0][j-1];
-13        }
-14        for(int i=1;i<n;i++){
-15            for(int j=1;j<m;j++){
-16                if(obs[i][j]==0){
-17                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
-18                }
-19            }
-20        }
-21        return dp[n-1][m-1];
-22    }
-23}
+11        return dp[m-1][n-1];
+12    }
+13}
