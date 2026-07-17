aba33c14e531971c42c413503dcbb2cefb0ceb0b
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-17 14:05:48 +0530

    File updated at 17/07/2026, 14:05:49

--- 64-MinimumPathSum/64-MinimumPathSum.java
+++ 64-MinimumPathSum/64-MinimumPathSum.java
@@ -1,20 +1,22 @@
-// Last updated: 17/07/2026, 14:02:00
+// Last updated: 17/07/2026, 14:05:49
 1class Solution {
-2    public int  recur(int i,int j,int[][] grid,int[][] dp){
-3        if(i==0 && j==0)    return dp[i][j]=grid[i][j];
-4        if(i<0 || j<0)    return Integer.MAX_VALUE;
-5        if(dp[i][j]!=-1)    return dp[i][j];
-6        int up=recur(i-1,j,grid,dp);
-7        int left=recur(i,j-1,grid,dp);
-8        int min=Math.min(up,left);
-9        if(min==Integer.MAX_VALUE)  return min;
+2    public int recur(int i,int j,int n,int m,int[][] grid,int[][] dp){
+3        if(i==n-1 && j==m-1) return dp[i][j]=grid[i][j];
+4        if(i>=n || j>=m) return  Integer.MAX_VALUE;
+5        if(dp[i][j]!=-1) return dp[i][j];
+6        int down=recur(i+1,j,n,m,grid,dp);
+7        int right=recur(i,j+1,n,m,grid,dp);
+8        int min=Math.min(down,right);
+9        if(min==Integer.MAX_VALUE) return min;
 10        return dp[i][j]=grid[i][j]+min;
 11    }
 12    public int minPathSum(int[][] grid) {
 13        int n=grid.length;
 14        int m=grid[0].length;
 15        int[][] dp=new int[n][m];
-16        for(int i=0;i<n;i++)    Arrays.fill(dp[i],-1);
-17        return recur(n-1,m-1,grid,dp);
-18    }
-19}
+16        for(int i=0;i<n;i++){
+17            Arrays.fill(dp[i],-1);
+18        }
+19        return recur(0,0,n,m,grid,dp);
+20    }
+21}
