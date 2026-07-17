a762c855484808920cf92455ae091f78543e4a64
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-17 10:11:32 +0530

    File updated at 17/07/2026, 10:11:33

--- 198-HouseRobber/198-HouseRobber.java
+++ 198-HouseRobber/198-HouseRobber.java
@@ -1,17 +1,17 @@
-// Last updated: 04/06/2026, 11:18:06
-class Solution {
-    public int recur(int ind,int[] nums,int[] dp){
-        if(ind>=nums.length){
-            return 0;
-        }
-        if(dp[ind]!=-1) return dp[ind];
-        int take=nums[ind]+recur(ind+2,nums,dp);
-        int notTake=0+recur(ind+1,nums,dp);
-        return dp[ind]=Math.max(take,notTake);
-    }
-    public int rob(int[] nums) {
-        int[] dp=new int[nums.length];
-        Arrays.fill(dp,-1);
-        return recur(0,nums,dp);
-    }
-}
+// Last updated: 17/07/2026, 10:11:33
+1class Solution {
+2    public int recur(int ind,int[] nums,int[] dp){
+3        if(ind>=nums.length){
+4            return 0;
+5        }
+6        if(dp[ind]!=-1) return dp[ind];
+7        int take=nums[ind]+recur(ind+2,nums,dp);
+8        int notTake=0+recur(ind+1,nums,dp);
+9        return dp[ind]=Math.max(take,notTake);
+10    }
+11    public int rob(int[] nums) {
+12        int[] dp=new int[nums.length];
+13        Arrays.fill(dp,-1);
+14        return recur(0,nums,dp);
+15    }
+16}
