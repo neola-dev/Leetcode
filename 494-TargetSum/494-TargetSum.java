fe7b42f65a583b58e4c3f111468b972c3534c77d
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-18 19:54:42 +0530

    File updated at 18/07/2026, 19:54:43

--- 494-TargetSum/494-TargetSum.java
+++ 494-TargetSum/494-TargetSum.java
@@ -1,25 +1,31 @@
-// Last updated: 04/06/2026, 11:15:58
-class Solution {
-    public int findTargetSumWays(int[] arr, int diff) {
-        int n=arr.length;
-        int tot=0;
-        for(int num:arr)   tot+=num;
-        int target=(tot-diff)/2;
-        if (tot - diff < 0 || (tot - diff) % 2 != 0) return 0;
-        int[][] dp=new int[n][target+1];
-        if(arr[0]==0)   dp[0][0]=2;
-        else dp[0][0]=1;
-        
-        if(arr[0]!=0 && arr[0]<=target) dp[0][arr[0]]=1;
-        
-        for(int i=1;i<n;i++){
-            for(int t=0;t<=target;t++){
-                int  notTake=dp[i-1][t];
-                int take=0;
-                if(arr[i]<=t)  take=dp[i-1][t-arr[i]];
-                dp[i][t]=take+notTake;
-            }
-        }
-        return dp[n-1][target];
-    }
-}
+// Last updated: 18/07/2026, 19:54:43
+1class Solution {
+2    public int recur(int ind,int[] arr,int target,int[][] dp){
+3        if(ind==0){
+4            if(arr[ind]==0 && target==0) return 2;
+5            if(target==0 || target==arr[0]) return 1;
+6            return 0;
+7        }
+8        if(dp[ind][target]!=-1) return dp[ind][target];
+9        int notPick=recur(ind-1,arr,target,dp);
+10        int pick=0;
+11        if(arr[ind]<=target) pick=recur(ind-1,arr,target-arr[ind],dp);
+12        dp[ind][target]=pick+notPick;
+13        return pick+notPick;
+14    }
+15    public int countPartitions(int[] arr, int diff) {
+16        int totSum=0;
+17        for(int num:arr) totSum+=num;
+18        if(totSum-diff<0 || (totSum-diff)%2!=0) return 0;
+19        int target=(totSum-diff)/2;
+20        int n=arr.length;
+21        int[][] dp=new int[n][target+1];
+22        for(int i=0;i<n;i++){
+23            Arrays.fill(dp[i],-1);
+24        }
+25        return recur(n-1,arr,target,dp);
+26    }
+27    public int findTargetSumWays(int[] nums, int target) {
+28        return countPartitions(nums,target);
+29    }
+30}
