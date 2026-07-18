e7a2ccd120aeb60fe5a28ba090487a6829628d92
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-18 13:07:23 +0530

    File updated at 18/07/2026, 13:07:23

--- 120-Triangle/120-Triangle.java
+++ 120-Triangle/120-Triangle.java
@@ -1,18 +1,18 @@
-// Last updated: 04/06/2026, 11:19:02
-class Solution {
-    public int minimumTotal(List<List<Integer>> triangle){
-        int n=triangle.size();
-        int[] dp=new int[n];
-        for(int j=0;j<triangle.get(n-1).size();j++){
-            dp[j]=triangle.get(n-1).get(j);
-        }
-        for(int i=n-2;i>=0;i--){
-            for(int j=0;j<=i;j++){
-                int down=dp[j];
-                int dia=dp[j+1];
-                dp[j]=triangle.get(i).get(j)+Math.min(down,dia);
-            }
-        }
-        return dp[0];
-    }
-}
+// Last updated: 18/07/2026, 13:07:23
+1class Solution {
+2    public int minimumTotal(List<List<Integer>> triangle) {
+3        int n=triangle.size();
+4        int[][] dp=new int[n][n];
+5        for(int j=0;j<triangle.get(n-1).size();j++){
+6            dp[n-1][j]=triangle.get(n-1).get(j);
+7        }
+8        for(int i=n-2;i>=0;i--){
+9            for(int j=0;j<triangle.get(i).size();j++){
+10                int down=dp[i+1][j];
+11                int diag=dp[i+1][j+1];
+12                dp[i][j]=triangle.get(i).get(j)+Math.min(down,diag);
+13            }
+14        }
+15        return dp[0][0];
+16    }
+17}
