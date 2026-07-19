9cc37b3e0fc7d0051adfcf345a7939bd96029701
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-20 00:21:32 +0530

    File updated at 20/07/2026, 00:21:32

--- 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
+++ 1143-LongestCommonSubsequence/1143-LongestCommonSubsequence.java
@@ -1,4 +1,4 @@
-// Last updated: 20/07/2026, 00:18:48
+// Last updated: 20/07/2026, 00:21:32
 1class Solution {
 2    public int longestCommonSubsequence(String text1, String text2) {
 3        int n = text1.length();
@@ -13,6 +13,24 @@
 12                }
 13            }
 14        }
-15        return dp[n][m];
-16    }
-17}
+15        //to print lcs
+16        int i=n;
+17        int j=m;
+18        StringBuilder sb=new StringBuilder();
+19        while(i>0 && j>0){
+20            if(text1.charAt(i-1)==text2.charAt(j-1)){
+21                sb.append(text1.charAt(i-1));
+22                i=i-1;
+23                j=j-1;
+24            }
+25            else if(dp[i-1][j]>dp[i][j-1]){
+26                i=i-1;
+27            }
+28            else{
+29                j=j-1;
+30            }
+31        }
+32        System.out.print(sb.reverse().toString());
+33        return dp[n][m];
+34    }
+35}
