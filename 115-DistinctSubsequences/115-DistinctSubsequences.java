ca7446778642a4ea01e96092a4a1f2b70adacf13
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-20 11:37:19 +0530

    File updated at 20/07/2026, 11:37:19

--- 115-DistinctSubsequences/115-DistinctSubsequences.java
+++ 115-DistinctSubsequences/115-DistinctSubsequences.java
@@ -1,29 +1,20 @@
-// Last updated: 04/06/2026, 11:19:08
-class Solution {
-    // public int recur(int i,int j,String s,String t,int[][] dp){
-    //     if(j<0) return 1;
-    //     if(i<0) return 0;
-    //     if(dp[i][j]!=-1)   return dp[i][j];
-    //     if(s.charAt(i)==t.charAt(j)){
-    //         return dp[i][j]=recur(i-1,j-1,s,t,dp)+recur(i-1,j,s,t,dp);
-    //     }
-    //     else{
-    //         return dp[i][j]=recur(i-1,j,s,t,dp);
-    //     }
-    // }
-    public int numDistinct(String s, String t) {
-        int n=s.length();
-        int m=t.length();
-        int[][] dp=new int[n+1][m+1];
-        for(int i=0;i<=n;i++)    dp[i][0]=1;
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s.charAt(i-1)==t.charAt(j-1))
-                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
-                else
-                    dp[i][j]=dp[i-1][j];
-            }
-        }
-        return dp[n][m];
-    }
-}
+// Last updated: 20/07/2026, 11:37:19
+1class Solution {
+2    public int numDistinct(String s, String t) {
+3        int n=s.length();
+4        int m=t.length();
+5        int[][] dp=new int[n+1][m+1];
+6        for(int i=0;i<=n;i++) dp[i][0]=1;
+7        for(int i=1;i<=n;i++){
+8            for(int j=1;j<=m;j++){
+9                if(s.charAt(i-1)==t.charAt(j-1)){
+10                    dp[i][j]=dp[i-1][j-1]+dp[i-1][j];
+11                }
+12                else{
+13                    dp[i][j]=dp[i-1][j];
+14                }
+15            }
+16        }
+17        return dp[n][m];
+18    }
+19}
