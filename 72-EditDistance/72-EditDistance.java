ba05e516f4148727a98ac37a68ed218ea0d32871
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-20 12:16:54 +0530

    File updated at 20/07/2026, 12:16:54

--- 72-EditDistance/72-EditDistance.java
+++ 72-EditDistance/72-EditDistance.java
@@ -1,29 +1,28 @@
-// Last updated: 04/06/2026, 11:19:49
-class Solution {
-    public int recur(int i,int j,String word1,String word2,int[][] dp){
-        if(i==0) return j;
-        if(j==0) return i;
-        if(dp[i][j]!=-1)    return dp[i][j];
-        if(word1.charAt(i-1)==word2.charAt(j-1)){
-            return dp[i][j]=recur(i-1,j-1,word1,word2,dp);
-        }
-        else{
-            //insert
-            int insert=1+recur(i,j-1,word1,word2,dp);
-            int delete=1+recur(i-1,j,word1,word2,dp);
-            int replace=1+recur(i-1,j-1,word1,word2,dp);
-            return dp[i][j]=Math.min(Math.min(insert,delete),replace);
-        }
-    }
-    public int minDistance(String word1, String word2) {
-        int n=word1.length();
-        int m=word2.length();
-        int[][] dp=new int[n+1][m+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=m;j++){
-                dp[i][j]=-1;
-            }
-        }
-        return recur(n,m,word1,word2,dp);
-    }
-}
+// Last updated: 20/07/2026, 12:16:54
+1class Solution {
+2    public int minDistance(String word1, String word2) {
+3        int n=word1.length();
+4        int m=word2.length();
+5        int[][] dp=new int[n+1][m+1];
+6        for(int i=0;i<=n;i++){
+7            dp[i][0]=i;
+8        }
+9        for(int j=0;j<=m;j++){
+10            dp[0][j]=j;
+11        }
+12        for(int i=1;i<=n;i++){
+13            for(int j=1;j<=m;j++){
+14                if(word1.charAt(i-1)==word2.charAt(j-1)){
+15                    dp[i][j]=dp[i-1][j-1];
+16                }
+17                else{
+18                    int insert=1+dp[i][j-1];
+19                    int delete=1+dp[i-1][j];
+20                    int update=1+dp[i-1][j-1];
+21                    dp[i][j]=Math.min(Math.min(insert,delete),update);
+22                }
+23            }
+24        }
+25        return dp[n][m];
+26    }
+27}
