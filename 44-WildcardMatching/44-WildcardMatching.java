a6c0f3fd2d3312fa7228501eb3deeecc3312e49a
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-20 14:23:49 +0530

    File updated at 20/07/2026, 14:23:50

--- 44-WildcardMatching/44-WildcardMatching.java
+++ 44-WildcardMatching/44-WildcardMatching.java
@@ -1,63 +1,32 @@
-// Last updated: 04/06/2026, 11:20:24
-class Solution {
-    // public boolean recur(int i,int j,String s,String p,int[][] dp){
-    //     if(i<0 && j<0)  return true;
-    //     if(i>=0 && j<0) return false;
-    //     if(j>=0 && i<0){
-    //         for(int ii=0;ii<=j;ii++){
-    //             if(p.charAt(ii)!='*')   return false;
-    //         }
-    //         return true;
-    //     }
-    //     if(dp[i][j]!=-1)    return dp[i][j]==1;
-    //     boolean ans;
-    //     if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?'){
-    //         ans=recur(i-1,j-1,s,p,dp);
-    //     }
-    //     else if(p.charAt(j)=='*'){
-    //         ans=recur(i-1,j,s,p,dp) || recur(i,j-1,s,p,dp);
-    //     }
-    //     else{
-    //         ans=false;
-    //     }    
-    //     dp[i][j]=ans==true?1:0;
-    //     return ans;
-    // }
-    public boolean isMatch(String s, String p) {
-        int n=s.length();
-        int m=p.length();
-        boolean[][] dp=new boolean[n+1][m+1];
-        dp[0][0]=true;
-        //string empty with pattern remaining
-        for(int j=1;j<=m;j++){
-            boolean ans=true;
-            for(int k=1;k<=j;k++){
-                if(p.charAt(k-1)!='*'){
-                    ans=false;
-                    break;
-                }    
-            }
-            dp[0][j]=ans;
-        }
-        //string remaning with pattern empty
-        for(int i=1;i<=n;i++){
-            dp[i][0]=false;
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                boolean ans;
-                if(s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='?'){
-                    ans=dp[i-1][j-1];
-                }
-                else if(p.charAt(j-1)=='*'){
-                    ans=dp[i-1][j] || dp[i][j-1];
-                }
-                else{
-                    ans=false;
-                }    
-                dp[i][j]=ans;
-            }    
-        }
-        return dp[n][m];
-    }
-}
+// Last updated: 20/07/2026, 14:23:50
+1class Solution {
+2    public boolean recur(int i,int j,String s,String p,int[][] dp){
+3        if(i<0 && j<0) return true;
+4        if(i>=0 && j<0) return false;
+5        if(i<0 && j>=0){
+6            for(int k=0;k<=j;k++){
+7                if(p.charAt(k)!='*') return false;
+8            }
+9            return true;
+10        }
+11        if(dp[i][j]!=-1) return dp[i][j]==1;
+12        boolean ans=false;
+13        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?'){
+14            ans=recur(i-1,j-1,s,p,dp);
+15        }
+16        else if(p.charAt(j)=='*'){
+17            ans=recur(i-1,j,s,p,dp) || recur(i,j-1,s,p,dp);
+18        }
+19        dp[i][j]=ans?1:0;
+20        return ans;
+21    }
+22    public boolean isMatch(String s, String p) {
+23        int n=s.length();
+24        int m=p.length();
+25        int[][] dp=new int[n+1][m+1];
+26        for(int i=0;i<=n;i++){
+27            Arrays.fill(dp[i],-1);
+28        }
+29        return recur(n-1,m-1,s,p,dp);
+30    }
+31}
