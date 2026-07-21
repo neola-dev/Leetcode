00369c363d26b3adce832d2624b59d916a089290
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-21 10:20:59 +0530

    File updated at 21/07/2026, 10:21:00

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
@@ -1,34 +1,24 @@
-// Last updated: 04/06/2026, 11:16:59
-class Solution {
-    //length of LIS using binary search in order to optimize time but the temp array is not the LIS array
-    public int lengthOfLIS(int[] nums) {
-        List<Integer> temp=new ArrayList<>();
-        temp.add(nums[0]);
-        int len=1;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]>temp.get(temp.size()-1)){
-                temp.add(nums[i]);
-                len++;
-            }
-            else{
-                int ind=lowerBound(nums[i],temp);
-                temp.set(ind,nums[i]);
-            }
-        }
-        return len;
-    }
-    public int lowerBound(int target,List<Integer> temp){
-        int low=0;
-        int high=temp.size()-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(temp.get(mid)>=target){
-                high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
-        }
-        return low;
-    }
-}
+// Last updated: 21/07/2026, 10:21:00
+1class Solution {
+2    public int memo(int ind,int prev,int[] nums,int[][] dp,int n){
+3        if(ind==n) return 0;
+4        if(dp[ind][prev+1]!=-1) return dp[ind][prev+1];
+5        int take=0;
+6        int notTake=0;
+7        if(prev==-1 || nums[prev]<nums[ind]){
+8            take=1+memo(ind+1,ind,nums,dp,n);
+9        }
+10        notTake=0+memo(ind+1,prev,nums,dp,n);
+11        return dp[ind][prev+1]=Math.max(take,notTake);
+12    }
+13    public int lengthOfLIS(int[] nums) {
+14        int n=nums.length;
+15        int[][] dp=new int[n][n+1];
+16        for(int i=0;i<n;i++){
+17            for(int j=0;j<=n;j++){
+18                dp[i][j]=-1;
+19            }
+20        }
+21        return memo(0,-1,nums,dp,n);
+22    }
+23}
