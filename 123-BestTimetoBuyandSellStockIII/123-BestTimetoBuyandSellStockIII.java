bbac0afb257daa8868a4967a79bfcfe0c80108f0
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-21 09:23:18 +0530

    File updated at 21/07/2026, 09:23:19

--- 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.java
+++ 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.java
@@ -1,28 +1,35 @@
-// Last updated: 21/07/2026, 09:10:37
+// Last updated: 21/07/2026, 09:23:19
 1class Solution {
-2    public int recur(int ind,int buy,int cap,int[] prices,int[][][] dp,int n){
-3        if(ind==n) return 0;
-4        if(cap==0) return 0;
-5        if(dp[ind][buy][cap]!=-1) return dp[ind][buy][cap];
-6        int profit=0;
-7        if(buy==1){
-8            profit=Math.max(-prices[ind]+recur(ind+1,0,cap,prices,dp,n),0+recur(ind+1,1,cap,prices,dp,n));
-9        }
-10        else{
-11            profit=Math.max(prices[ind]+recur(ind+1,1,cap-1,prices,dp,n),0+recur(ind+1,0,cap,prices,dp,n));
-12        }
-13        return dp[ind][buy][cap]=profit;
-14    }
+2    // public int recur(int ind,int buy,int cap,int[] prices,int[][][] dp,int n){
+3    //     if(ind==n) return 0;
+4    //     if(cap==0) return 0;
+5    //     if(dp[ind][buy][cap]!=-1) return dp[ind][buy][cap];
+6    //     int profit=0;
+7    //     if(buy==1){
+8    //         profit=Math.max(-prices[ind]+recur(ind+1,0,cap,prices,dp,n),0+recur(ind+1,1,cap,prices,dp,n));
+9    //     }
+10    //     else{
+11    //         profit=Math.max(prices[ind]+recur(ind+1,1,cap-1,prices,dp,n),0+recur(ind+1,0,cap,prices,dp,n));
+12    //     }
+13    //     return dp[ind][buy][cap]=profit;
+14    // }
 15    public int maxProfit(int[] prices) {
 16        int n=prices.length;
-17        int[][][] dp=new int[n][2][3]; //arr size,buy/sell(0,1),(0/1/2)transactions
-18        for(int i=0;i<n;i++){
-19            for(int j=0;j<2;j++){
-20                for(int k=0;k<3;k++){
-21                    dp[i][j][k]=-1;
-22                }
-23            }
-24        }
-25        return recur(0,1,2,prices,dp,n);
-26    }
-27}
+17        int[][][] dp=new int[n+1][2][3]; //arr size,buy/sell(0,1),(0/1/2)transactions
+18        for(int i=n-1;i>=0;i--){
+19            for(int buy=0;buy<2;buy++){
+20                for(int cap=1;cap<=2;cap++){
+21                    int profit=0;
+22                    if(buy==1){
+23                        profit=Math.max(-prices[i]+dp[i+1][0][cap],0+dp[i+1][1][cap]);
+24                    }
+25                    else{
+26                        profit=Math.max(prices[i]+dp[i+1][1][cap-1],0+dp[i+1][0][cap]);
+27                    }
+28                    dp[i][buy][cap]=profit;
+29                }
+30            }
+31        }
+32        return dp[0][1][2];
+33    }
+34}
