3bf6a09fcb3dcbd0805d02c68d9b9395d937da99
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-24 22:53:21 +0530

    File updated at 24/07/2026, 22:53:22

--- 39-CombinationSum/39-CombinationSum.java
+++ 39-CombinationSum/39-CombinationSum.java
@@ -1,22 +1,22 @@
-// Last updated: 04/06/2026, 11:20:29
-class Solution {
-    public static void findCombinations(int ind,int[] arr,int target,List<List<Integer>> res,List<Integer> ds){
-        if(ind==arr.length){
-            if(target==0){
-                res.add(new ArrayList<>(ds));
-            }
-            return;
-        }
-        if(arr[ind]<=target){
-            ds.add(arr[ind]);
-            findCombinations(ind,arr,target-arr[ind],res,ds);
-            ds.remove(ds.size()-1);
-        }
-        findCombinations(ind+1,arr,target,res,ds);
-    }
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res=new ArrayList<>();
-        findCombinations(0,candidates,target,res,new ArrayList<>());
-        return res;
-    }
-}
+// Last updated: 24/07/2026, 22:53:22
+1class Solution {
+2    public void recur(int ind,int[] candidates,int sum,int target,List<Integer> sub,List<List<Integer>> res){
+3        if(ind==candidates.length){
+4            if(sum==target){
+5                res.add(new ArrayList<>(sub));
+6            }
+7            return;
+8        }   
+9        if(sum+candidates[ind]<=target){ 
+10            sub.add(candidates[ind]);
+11            recur(ind,candidates,sum+candidates[ind],target,sub,res);
+12            sub.remove(sub.size()-1);
+13        }    
+14        recur(ind+1,candidates,sum,target,sub,res);
+15    }
+16    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+17        List<List<Integer>> res=new ArrayList<>();
+18        recur(0,candidates,0,target,new ArrayList<>(),res);
+19        return res;
+20    }
+21}
