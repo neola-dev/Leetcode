2699e1dd0b9ce29d30d28801fb905cf967919636
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-25 12:20:02 +0530

    File updated at 25/07/2026, 12:20:03

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
@@ -1,27 +1,12 @@
-// Last updated: 25/07/2026, 11:55:43
+// Last updated: 25/07/2026, 12:20:03
 1class Solution {
-2    public int numIslands(char[][] grid) {
-3        int isLandCnt=0;
-4        int n=grid.length;
-5        int m=grid[0].length;
-6        for(int i=0;i<n;i++){
-7            for(int j=0;j<m;j++){
-8                if(grid[i][j]=='1'){
-9                    isLandCnt++;
-10                    dfs(i,j,grid,n,m);
-11                }
-12            }
-13        }
-14        return isLandCnt;
-15    }
-16    public void dfs(int i,int j,char[][] grid,int n,int m){
-17        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]=='0'){
-18            return;
-19        }
-20        if(grid[i][j]=='1') grid[i][j]='0';
-21        dfs(i-1,j,grid,n,m);
-22        dfs(i+1,j,grid,n,m);
-23        dfs(i,j-1,grid,n,m);
-24        dfs(i,j+1,grid,n,m);
-25    }
-26}
+2    public int maxSubArray(int[] nums) {
+3        int currSum=nums[0];
+4        int maxSum=nums[0];
+5        for(int i=1;i<nums.length;i++){
+6            currSum=Math.max(currSum+nums[i],nums[i]);
+7            maxSum=Math.max(maxSum,currSum);
+8        }
+9        return maxSum;
+10    }
+11}
