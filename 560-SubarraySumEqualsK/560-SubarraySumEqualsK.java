56b0989eb8df17c45cac69fca695740319ee0386
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-25 11:55:42 +0530

    File updated at 25/07/2026, 11:55:43

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
@@ -1,16 +1,27 @@
-// Last updated: 25/07/2026, 09:24:42
+// Last updated: 25/07/2026, 11:55:43
 1class Solution {
-2    public int subarraySum(int[] nums, int k) {
-3        HashMap<Integer,Integer> map=new HashMap<>();
-4        int preSum=0;
-5        map.put(0,1);
-6        int cnt=0;
-7        for(int i=0;i<nums.length;i++){
-8            preSum+=nums[i];
-9            int rem=preSum-k;
-10            if(map.containsKey(rem)) cnt+=map.get(rem);
-11            map.put(preSum,map.getOrDefault(preSum,0)+1);
-12        }
-13        return cnt;
-14    }
-15}
+2    public int numIslands(char[][] grid) {
+3        int isLandCnt=0;
+4        int n=grid.length;
+5        int m=grid[0].length;
+6        for(int i=0;i<n;i++){
+7            for(int j=0;j<m;j++){
+8                if(grid[i][j]=='1'){
+9                    isLandCnt++;
+10                    dfs(i,j,grid,n,m);
+11                }
+12            }
+13        }
+14        return isLandCnt;
+15    }
+16    public void dfs(int i,int j,char[][] grid,int n,int m){
+17        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]=='0'){
+18            return;
+19        }
+20        if(grid[i][j]=='1') grid[i][j]='0';
+21        dfs(i-1,j,grid,n,m);
+22        dfs(i+1,j,grid,n,m);
+23        dfs(i,j-1,grid,n,m);
+24        dfs(i,j+1,grid,n,m);
+25    }
+26}
