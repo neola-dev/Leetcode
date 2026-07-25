fa86958ab64c7d7ef8c5f8b9d8eac082e9ab3510
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-25 09:18:12 +0530

    File updated at 25/07/2026, 09:18:13

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,32 +1,32 @@
-// Last updated: 04/06/2026, 11:20:18
-class Solution {
-    public List<List<String>> groupAnagrams(String[] strs) {
-        if(strs.length==0){
-            return new ArrayList<>();
-        }
-        HashMap<String,List<String>> map=new HashMap<>(); 
-        for(String word:strs){
-            int[] count=new int[26];
-            for(int i=0;i<word.length();i++){
-                count[word.charAt(i)-'a']++;
-            }
-            StringBuilder sb=new StringBuilder();
-            for(int c:count){
-                sb.append("#").append(c);
-            }
-            String key=sb.toString();
-            if(map.containsKey(key)){
-                map.get(key).add(word);
-            }
-            else{
-                map.put(key,new ArrayList<>());
-                map.get(key).add(word);
-            }
-        }
-        List<List<String>> res=new ArrayList<>();
-        for(List<String> val:map.values()){
-            res.add(val);
-        }
-        return res;
-    }
-}
+// Last updated: 25/07/2026, 09:18:13
+1class Solution {
+2    public List<List<String>> groupAnagrams(String[] strs) {
+3        List<List<String>> res=new ArrayList<>();
+4        HashMap<String,List<String>> map=new HashMap<>();
+5        for(int i=0;i<strs.length;i++){
+6            String word=strs[i];
+7            int[] freq=new int[26];
+8            for(int j=0;j<word.length();j++){
+9                char ch=word.charAt(j);
+10                freq[ch-'a']++;
+11            }
+12            StringBuilder key=new StringBuilder();
+13            for(int j=0;j<26;j++){
+14                key.append(freq[j]);
+15                key.append('#');
+16            }
+17            String keyStr=key.toString();
+18            if(map.containsKey(keyStr)){
+19                map.get(keyStr).add(word);
+20            }
+21            else{
+22                map.put(keyStr,new ArrayList<>());
+23                map.get(keyStr).add(word);
+24            }
+25        }
+26        for(List<String> pairs:map.values()){
+27            res.add(pairs);
+28        }
+29        return res;
+30    }
+31}
