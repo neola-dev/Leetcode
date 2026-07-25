4b55ded00472aee9c9e12c19d436832cecc8d5f6
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-25 11:47:41 +0530

    File updated at 25/07/2026, 11:47:42

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,32 +1,28 @@
-// Last updated: 25/07/2026, 09:18:13
+// Last updated: 25/07/2026, 11:47:42
 1class Solution {
-2    public List<List<String>> groupAnagrams(String[] strs) {
-3        List<List<String>> res=new ArrayList<>();
-4        HashMap<String,List<String>> map=new HashMap<>();
-5        for(int i=0;i<strs.length;i++){
-6            String word=strs[i];
-7            int[] freq=new int[26];
-8            for(int j=0;j<word.length();j++){
-9                char ch=word.charAt(j);
-10                freq[ch-'a']++;
-11            }
-12            StringBuilder key=new StringBuilder();
-13            for(int j=0;j<26;j++){
-14                key.append(freq[j]);
-15                key.append('#');
+2    public int minEatingSpeed(int[] piles, int h) {
+3        long min=Integer.MAX_VALUE;
+4        long max=Integer.MIN_VALUE;
+5        for(int i=0;i<piles.length;i++){
+6            min=Math.min(min,piles[i]);
+7            max=Math.max(max,piles[i]);
+8        }
+9        long low=1;
+10        long high=max;
+11        while(low<=high){
+12            long mid=(low+high)/2;
+13            int hours=0;
+14            for(int i=0;i<piles.length;i++){
+15                hours+=Math.ceil((double)piles[i]/mid);
 16            }
-17            String keyStr=key.toString();
-18            if(map.containsKey(keyStr)){
-19                map.get(keyStr).add(word);
+17            System.out.println(mid+" "+hours);
+18            if(hours<=h){
+19                high=mid-1;
 20            }
 21            else{
-22                map.put(keyStr,new ArrayList<>());
-23                map.get(keyStr).add(word);
-24            }
-25        }
-26        for(List<String> pairs:map.values()){
-27            res.add(pairs);
-28        }
-29        return res;
-30    }
-31}
+22                low=mid+1;
+23            }
+24        }
+25        return (int)low;
+26    }
+27}
