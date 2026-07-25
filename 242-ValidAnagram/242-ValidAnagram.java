3a605039e76e33ccea3736d6932852c5ce15ce25
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-25 09:08:03 +0530

    File updated at 25/07/2026, 09:08:04

--- 242-ValidAnagram/242-ValidAnagram.java
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -1,19 +1,17 @@
-// Last updated: 04/06/2026, 11:17:17
-class Solution {
-    public boolean isAnagram(String s, String t) { 
-        int[] arr=new int[26];
-        if(s.length()!=t.length()){
-            return false;
-        }
-        for(int i=0;i<s.length();i++){
-            arr[s.charAt(i)-'a']++;
-            arr[t.charAt(i)-'a']--;
-        }
-        for(int i=0;i<26;i++){
-            if(arr[i]!=0){
-                return false;
-            }
-        }
-        return true;
-    }
-}
+// Last updated: 25/07/2026, 09:08:04
+1class Solution {
+2    public boolean isAnagram(String s, String t) {
+3        int[] freq=new int[26];
+4        if(s.length()!=t.length()) return false;
+5        for(int i=0;i<s.length();i++){
+6            freq[s.charAt(i)-'a']++;
+7            freq[t.charAt(i)-'a']--;
+8        }
+9        for(int i=0;i<26;i++){
+10            if(freq[i]!=0){
+11                return false;
+12            }
+13        }
+14        return true;
+15    }
+16}
