57c970b79869dc47a1eab137bd672220e88afd31
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-25 13:52:43 +0530

    File updated at 25/07/2026, 13:52:45

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,28 +1,19 @@
-// Last updated: 25/07/2026, 11:47:42
+// Last updated: 25/07/2026, 13:52:45
 1class Solution {
-2    public int minEatingSpeed(int[] piles, int h) {
-3        long min=Integer.MAX_VALUE;
-4        long max=Integer.MIN_VALUE;
-5        for(int i=0;i<piles.length;i++){
-6            min=Math.min(min,piles[i]);
-7            max=Math.max(max,piles[i]);
-8        }
-9        long low=1;
-10        long high=max;
-11        while(low<=high){
-12            long mid=(low+high)/2;
-13            int hours=0;
-14            for(int i=0;i<piles.length;i++){
-15                hours+=Math.ceil((double)piles[i]/mid);
-16            }
-17            System.out.println(mid+" "+hours);
-18            if(hours<=h){
-19                high=mid-1;
-20            }
-21            else{
-22                low=mid+1;
-23            }
-24        }
-25        return (int)low;
-26    }
-27}
+2    public int maxSubarraySumCircular(int[] nums) {
+3        int maxSum=nums[0];
+4        int minSum=nums[0];
+5        int currMax=nums[0];
+6        int currMin=nums[0];
+7        int totSum=nums[0];
+8        for(int i=1;i<nums.length;i++){
+9            totSum+=nums[i];
+10            currMax=Math.max(currMax+nums[i],nums[i]);
+11            maxSum=Math.max(maxSum,currMax);
+12            currMin=Math.min(currMin+nums[i],nums[i]);
+13            minSum=Math.min(minSum,currMin);
+14        }
+15        if(maxSum<0) return maxSum;
+16        return Math.max(maxSum,totSum-minSum);
+17    }
+18}
