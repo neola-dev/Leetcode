d724b0443f4ac2a07de434c44ea0e4cdd31128e9
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-25 22:09:34 +0530

    File updated at 25/07/2026, 22:09:36

--- 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
+++ 168-ExcelSheetColumnTitle/168-ExcelSheetColumnTitle.java
@@ -1,13 +1,15 @@
-// Last updated: 04/06/2026, 11:18:21
-class Solution {
-    public String convertToTitle(int num) {
-        StringBuilder str=new StringBuilder();
-        while(num>0){
-            num--;
-            int rem=num%26;
-            str.append((char) (rem+'A'));
-            num/=26;
-        }
-        return str.reverse().toString();
-    }
-}
+// Last updated: 25/07/2026, 22:09:36
+1class Solution {
+2    public String convertToTitle(int num) {
+3        StringBuilder sb=new StringBuilder();
+4        while(num>0){
+5            num=num-1;
+6            int rem=num%26;
+7            System.out.println(rem);
+8            sb.append((char)(rem+'A'));
+9            System.out.println(num/26);
+10            num/=26;
+11        }
+12        return sb.reverse().toString();
+13    }
+14}
