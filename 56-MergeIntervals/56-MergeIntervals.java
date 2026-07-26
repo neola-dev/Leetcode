133dc37cfe7dc148d0ff1b4ea173159252b3ca25
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-26 11:10:16 +0530

    File updated at 26/07/2026, 11:10:15

--- 56-MergeIntervals/56-MergeIntervals.java
+++ 56-MergeIntervals/56-MergeIntervals.java
@@ -1,10 +1,10 @@
-// Last updated: 30/06/2026, 10:07:29
+// Last updated: 26/07/2026, 11:10:15
 1class Solution {
 2    public int[][] merge(int[][] intervals) {
 3        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
 4        List<int[]> res=new ArrayList<>();
 5        for(int[] inter:intervals){
-6            if(!res.isEmpty() && res.getLast()[1]>=inter[0]){
+6            if(!res.isEmpty() && inter[0]<=res.getLast()[1]){
 7                res.getLast()[1]=Math.max(res.getLast()[1],inter[1]);
 8            }
 9            else{
