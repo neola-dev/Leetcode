1f979a9c7e3849ee066e92d5357ad64e770e13f3
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-28 11:19:45 +0530

    File updated at 28/07/2026, 11:19:45

--- 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.java
+++ 153-FindMinimuminRotatedSortedArray/153-FindMinimuminRotatedSortedArray.java
@@ -1,18 +1,18 @@
-// Last updated: 14/06/2026, 18:30:29
+// Last updated: 28/07/2026, 11:19:45
 1class Solution {
 2    public int findMin(int[] nums) {
-3       int n=nums.length;
-4       int low=0;
-5       int high=n-1;
-6       while(low<high){
-7            int mid=(low+high)/2;
-8            if(nums[mid]>nums[high]){
-9                low=mid+1;
+3        int n=nums.length;
+4        int l=0;
+5        int h=n-1;
+6        while(l<h){
+7            int mid=l+(h-l)/2;
+8            if(nums[mid]>nums[h]){
+9                l=mid+1;
 10            }
 11            else{
-12                high=mid;
+12                h=mid;
 13            }
-14       }
-15       return nums[low];
+14        }
+15        return nums[l];
 16    }
 17}
