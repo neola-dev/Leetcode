c92c65db302af91d91f8871760845f904b3956e7
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-28 12:04:35 +0530

    File updated at 28/07/2026, 12:04:35

--- 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.java
+++ 98-ValidateBinarySearchTree/98-ValidateBinarySearchTree.java
@@ -1,28 +1,28 @@
-// Last updated: 04/06/2026, 11:19:28
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public boolean isValidBST(TreeNode root) {
-       return valid(root,Long.MIN_VALUE,Long.MAX_VALUE);
-    }
-    private boolean valid(TreeNode node,long min,long max){
-        if(node==null){
-            return true;
-        }
-        if(!(node.val>min && node.val<max)) return false;
-        return valid(node.left,min,node.val) && valid(node.right,node.val,max);
-    }
-}
+// Last updated: 28/07/2026, 12:04:35
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public boolean isValidBST(TreeNode root) {
+18       return valid(root,Long.MIN_VALUE,Long.MAX_VALUE);
+19    }
+20    private boolean valid(TreeNode node,long min,long max){
+21        if(node==null){
+22            return true;
+23        }
+24        if(!(node.val>min && node.val<max)) return false;
+25        return valid(node.left,min,node.val) && valid(node.right,node.val,max);
+26    }
+27}
