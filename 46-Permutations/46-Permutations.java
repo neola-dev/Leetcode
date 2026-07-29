070e8264270b1f88128ef2914ca79a7e63ab7e54
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-29 22:48:22 +0530

    File updated at 29/07/2026, 22:48:23

--- 46-Permutations/46-Permutations.java
+++ 46-Permutations/46-Permutations.java
@@ -1,31 +1,24 @@
-// Last updated: 27/06/2026, 12:45:20
+// Last updated: 29/07/2026, 22:48:23
 1class Solution {
-2    public void recur(String s,int ind,List<String> res,int n,StringBuilder sb,int parts){
-3        if(parts==4){
-4            if(ind==n){
-5                res.add(sb.toString());
-6            }
-7            return;
-8        }
-9        for(int len=1;len<=3;len++){
-10            if(ind+len>n)   break;
-11            String sub=s.substring(ind,ind+len);
-12            if(sub.length()>1 && sub.charAt(0)=='0') break;
-13            int num=Integer.parseInt(sub);
-14            if(num>255){
-15                break;
-16            }
-17            int size=sb.length();
-18            if(parts>0) sb.append('.'); 
-19            sb.append(sub);
-20            recur(s,ind+len,res,n,sb,parts+1);
-21            sb.setLength(size);
-22        }
-23    }
-24    public List<String> restoreIpAddresses(String s) {
-25        List<String> res=new ArrayList<>();
-26        int n=s.length();
-27        recur(s,0,res,n,new StringBuilder(),0);
-28        return res;
-29    }
-30}
+2    public void recur(int[] nums,List<List<Integer>> res,List<Integer> sub,boolean[] vis){
+3        if(sub.size()==nums.length){
+4            res.add(new ArrayList<>(sub));
+5            return;
+6        }
+7        for(int i=0;i<nums.length;i++){
+8            if(vis[i])  continue;
+9            sub.add(nums[i]);
+10            vis[i]=true;
+11            recur(nums,res,sub,vis);
+12            sub.remove(sub.size()-1);
+13            vis[i]=false;
+14        }
+15    }
+16    public List<List<Integer>> permute(int[] nums) {
+17        List<List<Integer>> res=new ArrayList<>();
+18        int n=nums.length;
+19        boolean[] vis=new  boolean[n];
+20        recur(nums,res,new ArrayList<>(),vis);
+21        return res;
+22    }
+23}
