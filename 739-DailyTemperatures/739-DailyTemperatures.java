a39ba4a6b89d0d0666d4515be4672eafc6866d75
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-29 22:58:14 +0530

    File updated at 29/07/2026, 22:58:15

--- 739-DailyTemperatures/739-DailyTemperatures.java
+++ 739-DailyTemperatures/739-DailyTemperatures.java
@@ -1,29 +1,16 @@
-// Last updated: 11/06/2026, 17:15:21
+// Last updated: 29/07/2026, 22:58:15
 1class Solution {
-2    public int[] asteroidCollision(int[] aster) {
-3        Stack<Integer> st=new Stack<>();
-4        for(int num:aster){
-5            if(num>0)   st.push(num);
-6            else{
-7                while(!st.isEmpty() && st.peek()>0 && st.peek()<Math.abs(num)){
-8                    st.pop();
-9                }
-10                if(!st.isEmpty() && st.peek()==Math.abs(num)){
-11                    st.pop();
-12                }
-13                else if(st.isEmpty() || st.peek()<0){
-14                    st.push(num);
-15                }
-16            }
-17        }
-18        List<Integer> res=new ArrayList<>();
-19        while(!st.isEmpty()){
-20            res.add(st.pop());
-21        }
-22        int[] ans=new int[res.size()];
-23        for(int i=0;i<res.size();i++){
-24            ans[i]=res.get(res.size()-1-i);
-25        }
-26        return ans;
-27    }
-28}
+2    public int[] dailyTemperatures(int[] temp) {
+3        int n=temp.length;
+4        int[] nge=new int[n];
+5        Stack<Integer> st=new Stack<>();
+6        for(int i=n-1;i>=0;i--){
+7            while(!st.isEmpty() && temp[st.peek()]<=temp[i]){
+8                st.pop();
+9            }
+10            nge[i]=st.isEmpty()?0:st.peek()-i;
+11            st.push(i);
+12        }
+13        return nge;
+14    }
+15}
