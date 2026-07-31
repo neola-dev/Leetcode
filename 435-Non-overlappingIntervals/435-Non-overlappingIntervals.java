1c4cb084769e8bcede5dee5cd1c6fe07901c186e
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-31 14:03:19 +0530

    File updated at 31/07/2026, 14:03:20

--- 435-Non-overlappingIntervals/435-Non-overlappingIntervals.java
+++ 435-Non-overlappingIntervals/435-Non-overlappingIntervals.java
@@ -1,31 +1,34 @@
-// Last updated: 31/07/2026, 13:58:59
+// Last updated: 31/07/2026, 14:03:20
 1/**
-2 * Definition for singly-linked list.
-3 * class ListNode {
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
 4 *     int val;
-5 *     ListNode next;
-6 *     ListNode(int x) {
-7 *         val = x;
-8 *         next = null;
-9 *     }
-10 * }
-11 */
-12public class Solution {
-13    public ListNode detectCycle(ListNode head) {
-14        ListNode slow=head;
-15        ListNode fast=head;
-16        while(fast!=null && fast.next!=null){
-17            slow=slow.next;
-18            fast=fast.next.next;
-19            if(slow==fast){
-20                slow=head;
-21                while(slow!=fast){
-22                    slow=slow.next;
-23                    fast=fast.next;
-24                }
-25                return slow;
-26            }
-27        }
-28        return null;
-29    }
-30}
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    List<Integer> res=new ArrayList<>();
+18    public List<Integer> rightSideView(TreeNode root) {
+19        dfs(0,root);
+20        return res;
+21    }
+22    public void dfs(int depth,TreeNode root){
+23        if(root==null){
+24            return;
+25        }
+26        if(res.size()==depth){
+27            res.add(root.val);
+28        }
+29        dfs(depth+1,root.right);
+30        dfs(depth+1,root.left);
+31    }
+32
+33}
