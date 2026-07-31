cc8594aad969c1274ab69573203ec851208006a8
Neola Kellsie P <neolapeter895@gmail.com>
2026-07-31 13:53:51 +0530

    File updated at 31/07/2026, 13:53:52

--- 435-Non-overlappingIntervals/435-Non-overlappingIntervals.java
+++ 435-Non-overlappingIntervals/435-Non-overlappingIntervals.java
@@ -1,17 +1,31 @@
-// Last updated: 31/07/2026, 13:51:00
-1class Solution {
-2    public int eraseOverlapIntervals(int[][] intervals) {
-3        Arrays.sort(intervals,(a,b)->a[1]-b[1]);
-4        int cnt=0;
-5        int prevEnd=intervals[0][1];
-6        for(int i=1;i<intervals.length;i++){
-7            if(intervals[i][0]<prevEnd){
-8                cnt++;
-9            }
-10            else{
-11                prevEnd=intervals[i][1];
-12            }
-13        }
-14        return cnt;
-15    }
-16}
+// Last updated: 31/07/2026, 13:53:52
+1/**
+2 * Definition for singly-linked list.
+3 * class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode(int x) {
+7 *         val = x;
+8 *         next = null;
+9 *     }
+10 * }
+11 */
+12public class Solution {
+13    public ListNode detectCycle(ListNode head) {
+14        ListNode slow=head;
+15        ListNode fast=head;
+16        while(fast!=null && fast.next!=null){
+17            slow=slow.next;
+18            fast=fast.next.next;
+19            if(slow==fast){
+20                slow=head;
+21                while(slow!=fast){
+22                    slow=slow.next;
+23                    fast=fast.next;
+24                }
+25                return slow;
+26            }
+27        }
+28        return null;
+29    }
+30}
