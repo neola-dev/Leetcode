472c97341666e44c50737a551f3c0e0b972356db
Neola Kellsie P <neolapeter895@gmail.com>
2026-08-01 23:27:18 +0530

    File updated at 01/08/2026, 23:27:19

--- 37-SudokuSolver/37-SudokuSolver.java
+++ 37-SudokuSolver/37-SudokuSolver.java
@@ -1,33 +1,28 @@
-// Last updated: 01/08/2026, 23:19:39
+// Last updated: 01/08/2026, 23:27:19
 1class Solution {
-2    public void solveSudoku(char[][] board) {
-3        solve(board);
-4    }
-5    public boolean solve(char[][] board){
-6        int n=board.length;
-7        int m=board[0].length;
-8        for(int i=0;i<n;i++){
-9            for(int j=0;j<m;j++){
-10                if(board[i][j]=='.'){
-11                    for(char ch='1';ch<='9';ch++){
-12                        if(safe(i,j,board,ch)){
-13                            board[i][j]=ch;
-14                            if(solve(board)) return true;
-15                            else board[i][j]='.';
-16                        }
-17                    }
-18                    return false;
-19                }
-20            }
-21        }
-22        return true;
-23    }
-24    public boolean safe(int row,int col,char[][] board,char ch){
-25        for(int i=0;i<board.length;i++){
-26            if(board[row][i]==ch)   return false;
-27            if(board[i][col]==ch)   return false;
-28            if(board[3*(row/3)+(i/3)][3*(col/3)+(i%3)]==ch) return false;
-29        }
-30        return true;
-31    }
-32}
+2    public int minDistance(String word1, String word2) {
+3        int n=word1.length();
+4        int m=word2.length();
+5        int[][] dp=new int[n+1][m+1];
+6        for(int i=0;i<=n;i++){
+7            dp[i][0]=i;
+8        }
+9        for(int j=0;j<=m;j++){
+10            dp[0][j]=j;
+11        }
+12        for(int i=1;i<=n;i++){
+13            for(int j=1;j<=m;j++){
+14                if(word1.charAt(i-1)==word2.charAt(j-1)){
+15                    dp[i][j]=dp[i-1][j-1];
+16                }
+17                else{
+18                    int insert=1+dp[i][j-1];
+19                    int delete=1+dp[i-1][j];
+20                    int update=1+dp[i-1][j-1];
+21                    dp[i][j]=Math.min(Math.min(insert,delete),update);
+22                }
+23            }
+24        }
+25        return dp[n][m];
+26    }
+27}
