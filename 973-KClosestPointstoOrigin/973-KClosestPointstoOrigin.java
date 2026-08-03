e0963a330caae108d2a2502a0a682199fdd47a61
Neola Kellsie P <neolapeter895@gmail.com>
2026-08-03 08:47:40 +0530

    File updated at 03/08/2026, 08:47:41

--- 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.java
+++ 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.java
@@ -1,17 +1,35 @@
-// Last updated: 03/08/2026, 08:42:58
-1class Solution {
-2    public int[][] kClosest(int[][] points, int k) {
-3        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->(b[0]*b[0]+b[1]*b[1])-(a[0]*a[0]+a[1]*a[1]));
-4        for(int[] p:points){
-5            pq.add(p);
-6            if(pq.size()>k){
-7                pq.poll();
-8            }
-9        }
-10        int[][] res=new int[k][2];
-11        for(int i=0;i<k;i++){
-12            res[i]=pq.poll();
-13        }
-14        return res;
-15    }
-16}
+// Last updated: 03/08/2026, 08:47:41
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    int cnt=0;
+18    int ans=-1;
+19    public int kthSmallest(TreeNode root, int k) {
+20        int cnt=1;
+21        find(root,k);
+22        return ans;
+23    }
+24    public void find(TreeNode root,int k){
+25        if(root==null) return;
+26        find(root.left,k);
+27        cnt++;
+28        if(cnt==k){
+29            ans=root.val;
+30            return;
+31        }
+32        find(root.right,k);
+33    }
+34}
