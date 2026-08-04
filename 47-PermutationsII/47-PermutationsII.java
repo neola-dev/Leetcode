149067aec2354af73c3779ddc4e2adee92e07a51
Neola Kellsie P <neolapeter895@gmail.com>
2026-08-04 22:07:07 +0530

    File updated at 04/08/2026, 22:07:08

--- 47-PermutationsII/47-PermutationsII.java
+++ 47-PermutationsII/47-PermutationsII.java
@@ -1,26 +1,12 @@
-// Last updated: 04/08/2026, 21:30:50
+// Last updated: 04/08/2026, 22:07:08
 1class Solution {
-2    public void recur(int[] nums,List<List<Integer>> res,List<Integer> sub,boolean[] vis){
-3        if(sub.size()==nums.length){
-4            res.add(new ArrayList<>(sub));
-5            return;
-6        }
-7        for(int i=0;i<nums.length;i++){
-8            if(vis[i])  continue;
-9            if(i>0 && nums[i]==nums[i-1] && !vis[i-1])   continue;
-10            sub.add(nums[i]);
-11            vis[i]=true;
-12            recur(nums,res,sub,vis);
-13            sub.remove(sub.size()-1);
-14            vis[i]=false;
-15        }
-16    }
-17    public List<List<Integer>> permuteUnique(int[] nums) {
-18        List<List<Integer>> res=new ArrayList<>();
-19        int n=nums.length;
-20        Arrays.sort(nums);
-21        boolean[] vis=new  boolean[n];
-22        recur(nums,res,new ArrayList<>(),vis);
-23        return res;
-24    }
-25}
+2    public int maxSubArray(int[] nums) {
+3        int currSum=nums[0];
+4        int maxSum=nums[0];
+5        for(int i=1;i<nums.length;i++){
+6            currSum=Math.max(currSum+nums[i],nums[i]);
+7            maxSum=Math.max(maxSum,currSum);
+8        }
+9        return maxSum;
+10    }
+11}
