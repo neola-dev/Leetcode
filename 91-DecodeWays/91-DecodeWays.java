e33a763b9b02851959ad7fde5470c1ee4710c833
Neola Kellsie P <neolapeter895@gmail.com>
2026-08-10 22:45:43 +0530

    File updated at 10/08/2026, 22:45:45

--- 91-DecodeWays/91-DecodeWays.java
+++ 91-DecodeWays/91-DecodeWays.java
@@ -1,24 +1,22 @@
-// Last updated: 10/08/2026, 21:32:45
+// Last updated: 10/08/2026, 22:45:45
 1class Solution {
-2    public int numDecodings(String s) {
-3        int n=s.length();
-4        int[] dp=new int[n];
-5        Arrays.fill(dp,-1);
-6        return find(0,s,n,dp);
-7    }
-8    public int find(int ind,String s,int n,int[] dp){
-9        if(ind==n) return 1;
-10        if(s.charAt(ind)=='0') return 0;
-11        if(dp[ind]!=-1) return dp[ind];
-12        int ways=0;
-13        ways+=find(ind+1,s,n,dp);
-14        if(ind+1<n){
-15            int num=(s.charAt(ind)-'0')*10+(s.charAt(ind+1)-'0');
-16            System.out.println(num);
-17            if(num>=10 && num<=26){
-18                ways+=find(ind+2,s,n,dp);
-19            }
-20        }
-21        return dp[ind]=ways;
-22    }
-23}
+2    public long countSubarrays(int[] nums, int k) {
+3        int max=0;
+4        for(int num:nums){
+5            max=Math.max(num,max);
+6        }
+7        int l=0,r=0;
+8        long cnt=0;
+9        long countMax=0;
+10        while(r<nums.length){
+11            if(nums[r]==max) countMax++;
+12            while(countMax>=k){
+13                if(nums[l]==max) countMax--;
+14                l++;
+15            }
+16            cnt+=l;
+17            r++;
+18        }
+19        return cnt;
+20    }
+21}
