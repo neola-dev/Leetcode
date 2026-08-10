79fe3387045c3b3d744aa57710b1eaf5e5e73d7b
Neola Kellsie P <neolapeter895@gmail.com>
2026-08-10 21:32:44 +0530

    File updated at 10/08/2026, 21:32:45

--- 91-DecodeWays/91-DecodeWays.java
+++ 91-DecodeWays/91-DecodeWays.java
@@ -1,25 +1,24 @@
-// Last updated: 12/07/2026, 11:02:35
+// Last updated: 10/08/2026, 21:32:45
 1class Solution {
-2    public int findWays(int ind,String s,int[] dp){
-3        if(ind==s.length()){
-4            return 1;
-5        }
-6        if(s.charAt(ind)=='0'){
-7            return 0;
-8        }
-9        if(dp[ind]!=-1) return dp[ind];
-10        int ways=findWays(ind+1,s,dp);
-11        if(ind+1<s.length()){
-12            int num=(s.charAt(ind)-'0')*10+(s.charAt(ind+1)-'0');
-13            if(num>=10 && num<=26){
-14                ways+=findWays(ind+2,s,dp);
-15            }
-16        }
-17        return dp[ind]=ways;
-18    }
-19    public int numDecodings(String s) {
-20        int[] dp=new int[s.length()];
-21        Arrays.fill(dp,-1);
-22        return findWays(0,s,dp);
-23    }
-24}
+2    public int numDecodings(String s) {
+3        int n=s.length();
+4        int[] dp=new int[n];
+5        Arrays.fill(dp,-1);
+6        return find(0,s,n,dp);
+7    }
+8    public int find(int ind,String s,int n,int[] dp){
+9        if(ind==n) return 1;
+10        if(s.charAt(ind)=='0') return 0;
+11        if(dp[ind]!=-1) return dp[ind];
+12        int ways=0;
+13        ways+=find(ind+1,s,n,dp);
+14        if(ind+1<n){
+15            int num=(s.charAt(ind)-'0')*10+(s.charAt(ind+1)-'0');
+16            System.out.println(num);
+17            if(num>=10 && num<=26){
+18                ways+=find(ind+2,s,n,dp);
+19            }
+20        }
+21        return dp[ind]=ways;
+22    }
+23}
