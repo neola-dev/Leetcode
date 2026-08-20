bfbf4e150900ca7ffb622860237e7887549b9b50
Neola Kellsie P <neolapeter895@gmail.com>
2026-08-20 14:55:46 +0530

    File updated at 20/08/2026, 14:55:53

--- 1054-DistantBarcodes/1054-DistantBarcodes.java
+++ 1054-DistantBarcodes/1054-DistantBarcodes.java
@@ -1,33 +1,29 @@
-// Last updated: 20/08/2026, 10:47:28
+// Last updated: 20/08/2026, 14:55:53
 1class Solution {
-2    public int[] rearrangeBarcodes(int[] barcodes) {
-3        int n=barcodes.length;
-4        int[] ans=new int[n];
-5        HashMap<Integer,Integer> map=new HashMap<>();
-6        for(int bc:barcodes){
-7            map.put(bc,map.getOrDefault(bc,0)+1);
-8        }
-9        PriorityQueue<int[]> pq=new PriorityQueue<>((a,b)->b[1]-a[1]);
-10        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-11            pq.offer(new int[]{entry.getKey(),entry.getValue()});
-12        }
-13        int prev=-1;
-14        for(int i=0;i<n;i++){
-15            int[] curr=pq.poll();
-16            if(prev==curr[0]){
-17                int[] next=pq.poll();
-18                ans[i]=next[0];
-19                next[1]--;
-20                if(next[1]>0) pq.offer(next);
-21                pq.offer(curr);
-22            }
-23            else{
-24                ans[i]=curr[0];
-25                curr[1]--;
-26                if(curr[1]>0) pq.offer(curr);
-27            }
-28            prev=ans[i];
-29        }
-30        return ans;
-31    }
-32}
+2    public List<Integer> luckyNumbers(int[][] matrix) {
+3        List<Integer> ans=new ArrayList<>();
+4        int n=matrix.length;
+5        int m=matrix[0].length;
+6        for(int i=0;i<n;i++){
+7            int min=Integer.MAX_VALUE;
+8            int minC=-1;
+9            for(int j=0;j<m;j++){
+10                if(matrix[i][j]<min){
+11                    min=Math.min(min,matrix[i][j]);
+12                    minC=j;
+13                }    
+14            }
+15            int max=Integer.MIN_VALUE;
+16            for(int k=0;k<n;k++){
+17                if(matrix[k][minC]>max){
+18                    max=matrix[k][minC];
+19                }
+20            }
+21
+22            if(min==max){
+23                ans.add(min);
+24            }
+25        }
+26        return ans;
+27    }
+28}
