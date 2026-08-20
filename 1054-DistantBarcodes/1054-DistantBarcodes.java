89877425cdd49fcc96d0e5b20ca3bed324f046bb
Neola Kellsie P <neolapeter895@gmail.com>
2026-08-20 15:21:42 +0530

    File updated at 20/08/2026, 15:21:49

--- 1054-DistantBarcodes/1054-DistantBarcodes.java
+++ 1054-DistantBarcodes/1054-DistantBarcodes.java
@@ -1,29 +1,8 @@
-// Last updated: 20/08/2026, 14:55:53
+// Last updated: 20/08/2026, 15:21:49
 1class Solution {
-2    public List<Integer> luckyNumbers(int[][] matrix) {
-3        List<Integer> ans=new ArrayList<>();
-4        int n=matrix.length;
-5        int m=matrix[0].length;
-6        for(int i=0;i<n;i++){
-7            int min=Integer.MAX_VALUE;
-8            int minC=-1;
-9            for(int j=0;j<m;j++){
-10                if(matrix[i][j]<min){
-11                    min=Math.min(min,matrix[i][j]);
-12                    minC=j;
-13                }    
-14            }
-15            int max=Integer.MIN_VALUE;
-16            for(int k=0;k<n;k++){
-17                if(matrix[k][minC]>max){
-18                    max=matrix[k][minC];
-19                }
-20            }
-21
-22            if(min==max){
-23                ans.add(min);
-24            }
-25        }
-26        return ans;
-27    }
-28}
+2    public int findClosest(int x, int y, int z) {
+3        if(Math.abs(z-x)<Math.abs(y-z)) return 1;
+4        else if(Math.abs(z-x)>Math.abs(y-z)) return 2;
+5        return 0;
+6    }
+7}
