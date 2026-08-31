accacdd4a073e44d4d099d1be21a397cb841230d
Neola Kellsie P <neolapeter895@gmail.com>
2026-08-31 09:25:41 +0530

    File updated at 31/08/2026, 09:25:42

--- 93-RestoreIPAddresses/93-RestoreIPAddresses.java
+++ 93-RestoreIPAddresses/93-RestoreIPAddresses.java
@@ -1,27 +1,18 @@
-// Last updated: 31/08/2026, 09:00:55
+// Last updated: 31/08/2026, 09:25:42
 1class Solution {
-2    public void recur(String s,int ind,List<String> res,int n,StringBuilder sb,int parts){
-3        if(parts==4){
-4            if(ind==n) res.add(sb.toString());
-5            return;
-6        }
-7        for(int len=1;len<=3;len++){
-8            if(ind+len>n) break;
-9            String sub=s.substring(ind,ind+len);
-10            if(sub.length()>1 && sub.charAt(0)=='0') break;
-11            int num=Integer.parseInt(sub);
-12            if(num>255) break;
-13            int size=sb.length();
-14            if(parts>0) sb.append('.');
-15            sb.append(sub);
-16            recur(s,ind+len,res,n,sb,parts+1);
-17            sb.setLength(size);
-18        }
-19    }
-20    public List<String> restoreIpAddresses(String s) {
-21        List<String> res=new ArrayList<>();
-22        int n=s.length();
-23        recur(s,0,res,n,new StringBuilder(),0);
-24        return res;  
-25    }
-26}
+2    public boolean wordBreak(String s, List<String> wordDict) {
+3        HashSet<String> set=new HashSet<>(wordDict);
+4        int n=s.length();
+5        boolean[] dp=new boolean[n+1];
+6        dp[0]=true;
+7        for(int i=1;i<=n;i++){
+8            for(int j=0;j<i;j++){
+9                if(dp[j] && set.contains(s.substring(j,i))){
+10                    dp[i]=true;
+11                    break;
+12                }
+13            }
+14        }
+15        return dp[n];
+16    }
+17}
