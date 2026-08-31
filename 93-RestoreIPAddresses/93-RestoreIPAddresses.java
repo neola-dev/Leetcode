3af9f15d8d1d4ed73d9f148f8d3b7afb38fb6a19
Neola Kellsie P <neolapeter895@gmail.com>
2026-08-31 10:09:19 +0530

    File updated at 31/08/2026, 10:09:20

--- 93-RestoreIPAddresses/93-RestoreIPAddresses.java
+++ 93-RestoreIPAddresses/93-RestoreIPAddresses.java
@@ -1,18 +1,25 @@
-// Last updated: 31/08/2026, 09:25:42
+// Last updated: 31/08/2026, 10:09:20
 1class Solution {
-2    public boolean wordBreak(String s, List<String> wordDict) {
-3        HashSet<String> set=new HashSet<>(wordDict);
-4        int n=s.length();
-5        boolean[] dp=new boolean[n+1];
-6        dp[0]=true;
-7        for(int i=1;i<=n;i++){
-8            for(int j=0;j<i;j++){
-9                if(dp[j] && set.contains(s.substring(j,i))){
-10                    dp[i]=true;
-11                    break;
-12                }
-13            }
-14        }
-15        return dp[n];
-16    }
-17}
+2    public void recur(int ind,String s,List<String> res,HashSet<String> set,StringBuilder sb){
+3        if(ind>=s.length()){
+4            res.add(sb.toString());
+5            return;
+6        }
+7        for(int i=ind;i<s.length();i++){
+8            String part=s.substring(ind,i+1);
+9            if(set.contains(part)){
+10                int oldSize=sb.length();
+11                sb.append(part);
+12                if(i!=s.length()-1) sb.append(" ");
+13                recur(i+1,s,res,set,sb);
+14                sb.setLength(oldSize);
+15            }
+16        }
+17    }
+18    public List<String> wordBreak(String s, List<String> wordDict) {
+19        List<String> res=new ArrayList<>();
+20        HashSet<String> set=new HashSet<>(wordDict);
+21        recur(0,s,res,set,new StringBuilder());
+22        return res;
+23    }
+24}
