30eae84973b2cbcf2fcff32ae034db0d07a89a0e
Neola Kellsie P <neolapeter895@gmail.com>
2026-08-31 11:29:11 +0530

    File updated at 31/08/2026, 11:29:13

--- 93-RestoreIPAddresses/93-RestoreIPAddresses.java
+++ 93-RestoreIPAddresses/93-RestoreIPAddresses.java
@@ -1,25 +1,19 @@
-// Last updated: 31/08/2026, 10:09:20
+// Last updated: 31/08/2026, 11:29:13
 1class Solution {
-2    public void recur(int ind,String s,List<String> res,HashSet<String> set,StringBuilder sb){
-3        if(ind>=s.length()){
-4            res.add(sb.toString());
-5            return;
-6        }
-7        for(int i=ind;i<s.length();i++){
-8            String part=s.substring(ind,i+1);
-9            if(set.contains(part)){
-10                int oldSize=sb.length();
-11                sb.append(part);
-12                if(i!=s.length()-1) sb.append(" ");
-13                recur(i+1,s,res,set,sb);
-14                sb.setLength(oldSize);
-15            }
-16        }
+2    public int canCompleteCircuit(int[] gas, int[] cost) {
+3        int totGas=0;
+4        for(int num:gas) totGas+=num;
+5        int totCost=0;
+6        for(int num:cost) totCost+=num;
+7        if(totGas<totCost) return -1;
+8        int currGas=0,startInd=0;
+9        for(int i=0;i<gas.length;i++){
+10            currGas+=(gas[i]-cost[i]);
+11            if(currGas<0){
+12                startInd=i+1;
+13                currGas=0;
+14            }
+15        }
+16        return startInd;
 17    }
-18    public List<String> wordBreak(String s, List<String> wordDict) {
-19        List<String> res=new ArrayList<>();
-20        HashSet<String> set=new HashSet<>(wordDict);
-21        recur(0,s,res,set,new StringBuilder());
-22        return res;
-23    }
-24}
+18}
