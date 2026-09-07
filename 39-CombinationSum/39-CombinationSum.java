6f012c4ee6acac013504c95afe6686a92e9704a9
Neola Kellsie P <neolapeter895@gmail.com>
2026-09-07 14:43:45 +0530

    File updated at 07/09/2026, 14:43:46

--- 39-CombinationSum/39-CombinationSum.java
+++ 39-CombinationSum/39-CombinationSum.java
@@ -1,23 +1,24 @@
-// Last updated: 07/09/2026, 14:38:54
+// Last updated: 07/09/2026, 14:43:46
 1class Solution {
-2    public void recur(int ind,int[] candidates,int target,List<List<Integer>> res,List<Integer> sub,int n){
-3        if(ind==n){
-4            if(target==0){
-5                res.add(new ArrayList<>(sub));
-6            }
-7            return;
-8        }
-9        if(candidates[ind]<=target){
-10            sub.add(candidates[ind]);
-11            recur(ind,candidates,target-candidates[ind],res,sub,n);
+2    public void recur(int[] nums,List<List<Integer>> res,List<Integer> sub,boolean[] vis,int n){
+3        if(sub.size()==n){
+4            res.add(new ArrayList<>(sub));
+5            return;
+6        }
+7        for(int i=0;i<n;i++){
+8            if(vis[i]) continue;
+9            sub.add(nums[i]);
+10            vis[i]=true;
+11            recur(nums,res,sub,vis,n);
 12            sub.remove(sub.size()-1);
-13        }
-14        recur(ind+1,candidates,target,res,sub,n);
+13            vis[i]=false;
+14        }
 15    }
-16    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+16    public List<List<Integer>> permute(int[] nums) {
 17        List<List<Integer>> res=new ArrayList<>();
-18        int n=candidates.length;
-19        recur(0,candidates,target,res,new ArrayList<>(),n);
-20        return res;
-21    }
-22}
+18        int n=nums.length;
+19        boolean[] vis=new boolean[n];
+20        recur(nums,res,new ArrayList<>(),vis,n);
+21        return res;
+22    }
+23}
