c5fb6c95a4696dafa2ed1fe175f78343869dda7c
Neola Kellsie P <neolapeter895@gmail.com>
2026-09-07 15:12:09 +0530

    File updated at 07/09/2026, 15:12:10

--- 56-MergeIntervals/56-MergeIntervals.java
+++ 56-MergeIntervals/56-MergeIntervals.java
@@ -1,21 +1,29 @@
-// Last updated: 07/09/2026, 15:08:43
+// Last updated: 07/09/2026, 15:12:10
 1class Solution {
-2    public int maxArea(int[] height) {
-3        int n=height.length;
+2    public int search(int[] nums, int target) {
+3        int n=nums.length;
 4        int l=0;
-5        int r=n-1;
-6        int max=Integer.MIN_VALUE;
-7        while(l<=r){
-8            int hei=Math.min(height[l],height[r]);
-9            int wid=r-l;
-10            max=Math.max(max,hei*wid);
-11            if(height[l]<height[r]){
-12                l++;
-13            }
-14            else{
-15                r--;
+5        int h=n-1;
+6        while(l<=h){
+7            int m=l+(h-l)/2;
+8            if(nums[m]==target) return m;
+9            if(nums[l]<=nums[m]){
+10                if(nums[l]<=target && target<=nums[m]){
+11                    h=m-1;
+12                }
+13                else{
+14                    l=m+1;
+15                }
 16            }
-17        }
-18        return max;
-19    }
-20}
+17            else{
+18                if(nums[m]<=target && target<=nums[h]){
+19                    l=m+1;
+20                }
+21                else{
+22                    h=m-1;
+23                }
+24            }
+25        }
+26        return -1;
+27    }
+28}
