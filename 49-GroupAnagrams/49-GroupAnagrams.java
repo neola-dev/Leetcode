d93daf2595bd302cc2d2620f3d307cd9ccb8da7e
Neola Kellsie P <neolapeter895@gmail.com>
2026-09-07 15:22:06 +0530

    File updated at 07/09/2026, 15:22:07

--- 49-GroupAnagrams/49-GroupAnagrams.java
+++ 49-GroupAnagrams/49-GroupAnagrams.java
@@ -1,19 +1,30 @@
-// Last updated: 25/07/2026, 13:52:45
+// Last updated: 07/09/2026, 15:22:07
 1class Solution {
-2    public int maxSubarraySumCircular(int[] nums) {
-3        int maxSum=nums[0];
-4        int minSum=nums[0];
-5        int currMax=nums[0];
-6        int currMin=nums[0];
-7        int totSum=nums[0];
-8        for(int i=1;i<nums.length;i++){
-9            totSum+=nums[i];
-10            currMax=Math.max(currMax+nums[i],nums[i]);
-11            maxSum=Math.max(maxSum,currMax);
-12            currMin=Math.min(currMin+nums[i],nums[i]);
-13            minSum=Math.min(minSum,currMin);
-14        }
-15        if(maxSum<0) return maxSum;
-16        return Math.max(maxSum,totSum-minSum);
-17    }
-18}
+2    public List<List<String>> groupAnagrams(String[] strs) {
+3        List<List<String>> res=new ArrayList<>();
+4        HashMap<String,List<String>> map=new HashMap<>();
+5        for(int i=0;i<strs.length;i++){
+6            String word=strs[i];
+7            int[] freq=new int[26];
+8            for(int j=0;j<word.length();j++){
+9                freq[word.charAt(j)-'a']++;
+10            }
+11            StringBuilder sb=new StringBuilder();
+12            for(int j=0;j<26;j++){
+13                sb.append(freq[j]);
+14                sb.append('#');
+15            }
+16            if(map.containsKey(sb.toString())){
+17                map.get(sb.toString()).add(word);
+18            }
+19            else{
+20                map.put(sb.toString(),new ArrayList<>());
+21                map.get(sb.toString()).add(word);
+22            }
+23        }
+24        for(List<String> pairs:map.values()){
+25            res.add(pairs);
+26        }
+27        return res;
+28    }
+29}
