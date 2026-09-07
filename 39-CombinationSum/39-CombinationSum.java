1bc34360e868c3a8ca058b4c99d51a154b8a1f23
Neola Kellsie P <neolapeter895@gmail.com>
2026-09-07 14:38:53 +0530

    File updated at 07/09/2026, 14:38:54

--- 39-CombinationSum/39-CombinationSum.java
+++ 39-CombinationSum/39-CombinationSum.java
@@ -1,22 +1,23 @@
-// Last updated: 24/07/2026, 22:53:22
+// Last updated: 07/09/2026, 14:38:54
 1class Solution {
-2    public void recur(int ind,int[] candidates,int sum,int target,List<Integer> sub,List<List<Integer>> res){
-3        if(ind==candidates.length){
-4            if(sum==target){
+2    public void recur(int ind,int[] candidates,int target,List<List<Integer>> res,List<Integer> sub,int n){
+3        if(ind==n){
+4            if(target==0){
 5                res.add(new ArrayList<>(sub));
 6            }
 7            return;
-8        }   
-9        if(sum+candidates[ind]<=target){ 
+8        }
+9        if(candidates[ind]<=target){
 10            sub.add(candidates[ind]);
-11            recur(ind,candidates,sum+candidates[ind],target,sub,res);
+11            recur(ind,candidates,target-candidates[ind],res,sub,n);
 12            sub.remove(sub.size()-1);
-13        }    
-14        recur(ind+1,candidates,sum,target,sub,res);
+13        }
+14        recur(ind+1,candidates,target,res,sub,n);
 15    }
 16    public List<List<Integer>> combinationSum(int[] candidates, int target) {
 17        List<List<Integer>> res=new ArrayList<>();
-18        recur(0,candidates,0,target,new ArrayList<>(),res);
-19        return res;
-20    }
-21}
+18        int n=candidates.length;
+19        recur(0,candidates,target,res,new ArrayList<>(),n);
+20        return res;
+21    }
+22}
