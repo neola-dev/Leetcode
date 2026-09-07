bd5d441832201bb4a8b9ed00bdb4603ce49e95a3
Neola Kellsie P <neolapeter895@gmail.com>
2026-09-07 14:46:52 +0530

    File updated at 07/09/2026, 14:46:53

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,16 +1,16 @@
-// Last updated: 04/06/2026, 11:21:16
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> hmap=new HashMap<>();
-        int[] res=new int[2];
-        for(int i=0;i<nums.length;i++){
-            int rem=target-nums[i];
-            if(hmap.containsKey(rem)){
-                res[0]=i;
-                res[1]=hmap.get(rem);
-            }
-            hmap.put(nums[i],i);
-        }
-        return res;
-    }
-}
+// Last updated: 07/09/2026, 14:46:53
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        HashMap<Integer,Integer> hmap=new HashMap<>();
+4        int[] res=new int[2];
+5        for(int i=0;i<nums.length;i++){
+6            int rem=target-nums[i];
+7            if(hmap.containsKey(rem)){
+8                res[0]=i;
+9                res[1]=hmap.get(rem);
+10            }
+11            hmap.put(nums[i],i);
+12        }
+13        return res;
+14    }
+15}
