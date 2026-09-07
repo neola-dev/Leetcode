915ab18b6264a9ace9c9c944b75347679caa12ca
Neola Kellsie P <neolapeter895@gmail.com>
2026-09-07 13:56:18 +0530

    File updated at 07/09/2026, 13:56:20

--- 200-NumberofIslands/200-NumberofIslands.java
+++ 200-NumberofIslands/200-NumberofIslands.java
@@ -1,20 +1,39 @@
-// Last updated: 07/09/2026, 13:47:23
-1class Solution {
-2    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-3        int n=image.length;
-4        int m=image[0].length;
-5        if(image[sr][sc]==color) return image;
-6        dfs(sr,sc,image,n,m,color,image[sr][sc]);
-7        return image;
-8    }
-9    public void dfs(int i,int j,int[][] image,int n,int m,int color,int ogColor){
-10        if(i<0 || j<0 || i>=n || j>=m || image[i][j]!=ogColor){
-11            return;
-12        }
-13        if(image[i][j]==ogColor) image[i][j]=color;
-14        dfs(i-1,j,image,n,m,color,ogColor);
-15        dfs(i+1,j,image,n,m,color,ogColor);
-16        dfs(i,j-1,image,n,m,color,ogColor);
-17        dfs(i,j+1,image,n,m,color,ogColor);
-18    }
-19}
+// Last updated: 07/09/2026, 13:56:20
+1class Pair{
+2    String word;
+3    int steps;
+4    Pair(String w,int s){
+5        this.word=w;
+6        this.steps=s;
+7    }
+8}
+9class Solution {
+10    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+11        Set<String> set=new HashSet<>(wordList);
+12        if(!set.contains(endWord)) return 0;
+13        Queue<Pair> q=new LinkedList<>();
+14        q.add(new Pair(beginWord,1));
+15        while(!q.isEmpty()){
+16            Pair curr=q.poll();
+17            String word=curr.word;
+18            int steps=curr.steps;
+19            if(word.equals(endWord)){
+20                return steps;
+21            }
+22            char[] arr=word.toCharArray();
+23            for(int i=0;i<arr.length;i++){
+24                char org=arr[i];
+25                for(char c='a';c<='z';c++){
+26                    arr[i]=c;
+27                    String newWord=new String(arr);
+28                    if(set.contains(newWord)){
+29                        q.add(new Pair(newWord,steps+1));
+30                        set.remove(newWord);
+31                    }
+32                }
+33                arr[i]=org;
+34            }
+35        }
+36        return 0;
+37    }
+38}
