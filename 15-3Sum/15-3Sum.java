ece4a6d578f87d32f6326d8f809fd2695d0e7509
Neola Kellsie P <neolapeter895@gmail.com>
2026-09-07 14:54:10 +0530

    File updated at 07/09/2026, 14:54:11

--- 15-3Sum/15-3Sum.java
+++ 15-3Sum/15-3Sum.java
@@ -1,8 +1,8 @@
-// Last updated: 26/07/2026, 11:00:32
+// Last updated: 07/09/2026, 14:54:11
 1class Solution {
 2    public List<List<Integer>> threeSum(int[] nums) {
-3        List<List<Integer>> res=new ArrayList<>();
-4        Arrays.sort(nums);
+3        Arrays.sort(nums);
+4        List<List<Integer>> res=new ArrayList<>();
 5        int n=nums.length;
 6        for(int i=0;i<n;i++){
 7            if(i!=0 && nums[i]==nums[i-1]) continue;
@@ -18,7 +18,7 @@
 17                    while(j<k && nums[j]==nums[j-1]){
 18                        j++;
 19                    }
-20                    while(j<k && nums[k+1]==nums[k]){
+20                    while(j<k && nums[k]==nums[k+1]){
 21                        k--;
 22                    }
 23                }
