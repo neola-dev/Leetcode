b2a49053fc762274a4a8d85dc1989462b298f336
Neola Kellsie P <neolapeter895@gmail.com>
2026-09-07 13:47:22 +0530

    File updated at 07/09/2026, 13:47:23

--- 200-NumberofIslands/200-NumberofIslands.java
+++ 200-NumberofIslands/200-NumberofIslands.java
@@ -1,29 +1,20 @@
-// Last updated: 07/09/2026, 13:40:06
+// Last updated: 07/09/2026, 13:47:23
 1class Solution {
-2    public int numIslands(char[][] grid) {
-3        int n=grid.length;
-4        int m=grid[0].length;
-5        int isLand=0;
-6        for(int i=0;i<n;i++){
-7            for(int j=0;j<m;j++){
-8                if(grid[i][j]=='1'){
-9                    isLand++;
-10                    dfs(i,j,grid,n,m);
-11                }
-12            }
-13        }
-14        return isLand;
-15    }
-16    public void dfs(int i,int j,char[][] grid,int n,int m){
-17        if(i<0 || j<0 || i>=n || j>=m || grid[i][j]=='0'){
-18            return;
-19        }
-20        if(grid[i][j]=='1'){
-21            grid[i][j]='0';
-22        }
-23        dfs(i-1,j,grid,n,m);
-24        dfs(i+1,j,grid,n,m);
-25        dfs(i,j-1,grid,n,m);
-26        dfs(i,j+1,grid,n,m);
-27    }
-28}
+2    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+3        int n=image.length;
+4        int m=image[0].length;
+5        if(image[sr][sc]==color) return image;
+6        dfs(sr,sc,image,n,m,color,image[sr][sc]);
+7        return image;
+8    }
+9    public void dfs(int i,int j,int[][] image,int n,int m,int color,int ogColor){
+10        if(i<0 || j<0 || i>=n || j>=m || image[i][j]!=ogColor){
+11            return;
+12        }
+13        if(image[i][j]==ogColor) image[i][j]=color;
+14        dfs(i-1,j,image,n,m,color,ogColor);
+15        dfs(i+1,j,image,n,m,color,ogColor);
+16        dfs(i,j-1,image,n,m,color,ogColor);
+17        dfs(i,j+1,image,n,m,color,ogColor);
+18    }
+19}
