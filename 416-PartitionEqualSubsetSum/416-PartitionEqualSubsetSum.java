402dcbdf913d2c4e129e67c2883b76eba7c05701
Neola Kellsie P <neolapeter895@gmail.com>
2026-09-08 22:27:06 +0530

    File updated at 08/09/2026, 22:27:06

--- 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
+++ 416-PartitionEqualSubsetSum/416-PartitionEqualSubsetSum.java
@@ -1,25 +1,25 @@
-// Last updated: 18/07/2026, 13:40:51
+// Last updated: 08/09/2026, 22:27:06
 1class Solution {
-2    public boolean memo(int ind,int t,int[] nums,int[][] dp){
+2    public boolean recur(int ind,int[] nums,int[][] dp,int t){
 3        if(t==0) return true;
 4        if(ind==0) return nums[ind]==t;
 5        if(dp[ind][t]!=-1) return dp[ind][t]==1;
-6        boolean notTake=memo(ind-1,t,nums,dp);
-7        boolean take=false;
-8        if(nums[ind]<=t) take=memo(ind-1,t-nums[ind],nums,dp);
-9        dp[ind][t]=(take || notTake)?1:0;
-10        return take || notTake;
+6        boolean notPick=recur(ind-1,nums,dp,t);
+7        boolean pick=false;
+8        if(nums[ind]<=t)    pick=recur(ind-1,nums,dp,t-nums[ind]);
+9        dp[ind][t]=pick || notPick?1:0;
+10        return pick || notPick;
 11    }
 12    public boolean canPartition(int[] nums) {
 13        int n=nums.length;
 14        int sum=0;
 15        for(int num:nums) sum+=num;
-16        if(sum%2!=0) return false;
-17        int target=sum/2;
-18        int[][] dp=new int[n][target+1];
+16        if(sum%2==1) return false;
+17        int t=sum/2;
+18        int[][] dp=new int[n][t+1];
 19        for(int i=0;i<n;i++){
 20            Arrays.fill(dp[i],-1);
 21        }
-22        return memo(n-1,target,nums,dp);
+22        return  recur(n-1,nums,dp,t);
 23    }
 24}
